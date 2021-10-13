d7a065623d67ad5fcc540b89f312df123803f506
Guilherme Leite <guilhermelax@hotmail.com>
2021-10-13 20:34:41 -0300

    mais exercicios feitos

--- fundamentos/bloco-3-introducao-a-html-e-css/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-3-introducao-a-html-e-css/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -29,3 +29,74 @@ if (d % 2 === 0 && d ==! 0) {
   console.log(`O valor de d é ${d} e ele é um ńumero negativo ou zero`);
 }
 
+let ar1 = Math.floor(Math.random()*90);
+let ar2 = Math.floor(Math.random()*90);
+let ar3 = Math.floor(Math.random()*90);
+
+
+console.log(ar1)
+console.log(ar2)
+console.log(ar3)
+
+if (ar1+ar2+ar3 > 180) {
+  console.log('é um triângulo')
+} else {console.log('não é um triângulo')}
+
+arr = ['pawn', 'bishop', 'knight', 'rook', 'queen', 'king']
+peca = arr[Math.floor(Math.random()*6)]
+
+switch (peca){
+  case 'pawn':
+  console.log('Pawns can only move forward');
+  break;
+
+  case 'bishop':
+  console.log('Bishops can move any number of squares diagonally.');
+  break;
+
+  case 'knight':
+  console.log('Knights can move only in an L-shape');
+  break;
+
+  case 'rook':
+  console.log('Rooks can move any number of squares, up and down and side to side.');
+  break;
+
+  case 'queen':
+  console.log('Queens can move any number of squares along ranks, files and diagonals.');
+  break;
+
+  case 'king':
+  console.log('Kings can move one square at a time in any direction.');
+  break;
+
+  default: console.log(`não deu certo, o valor de peça foi: ${peca}`);
+}
+
+
+// let notaPorcentagem = Math.floor(Math.random()*101);
+// console.log(notaPorcentagem)
+
+// switch (notaPorcentagem) {
+//   case notaPorcentagem >= 90:
+//     console.log('maior que 90')
+//     break;
+//   case notaPorcentagem <= 90:
+//     console.log('menor que 90')
+//     break;
+//   default:
+//     console.log('deu ruim')
+// }
+
+
+let num1 = 1;
+let num2 = 4;
+let num3 = 17;
+
+for (i=1; i<3; i++){
+  if (num[i] % 2 === 0)
+  return true;
+  else {
+    return false;
+  }
+}
