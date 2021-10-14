7b51b005745a83c7ca176799c24fbe626d45c629
Guilherme Leite <guilhermelax@hotmail.com>
2021-10-14 12:35:56 -0300

    finalizando exercícios 4.1

--- fundamentos/bloco-3-introducao-a-html-e-css/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
+++ fundamentos/bloco-3-introducao-a-html-e-css/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/script.js
@@ -1,102 +1,295 @@
-const a = 10;
-const b = 5;
-const c = 3;
-const d = Math.floor(Math.random()*11);
+// const a = 10;
+// const b = 5;
+// const c = 3;
+// const d = Math.floor(Math.random()*11);
 
-console.log('Soma: '+(a+b));
-console.log('Subtração: ' + (a-b));
-console.log('Multiplicação: ' + (a*b))
-console.log('Divisão: ' +(a/b))
-console.log('Modulo: ' + (a%b))
+// console.log('Soma: '+(a+b));
+// console.log('Subtração: ' + (a-b));
+// console.log('Multiplicação: ' + (a*b))
+// console.log('Divisão: ' +(a/b))
+// console.log('Modulo: ' + (a%b))
 
-if (a > b) {
-  console.log('O maior valor é a')
-} else {
-  console.log('O maior valor é b')
-}
-
-if (a > b && a > c) {
-  console.log('O maior número é o a')
-} else if (b > a && b > c) {
-  console.log('O maior ńumero é o b')
-} else if (c > a && c > b) {
-  console.log('O maior número é o c')
-} else {console.log('Mais de um dos números é igual')}
-
-if (d % 2 === 0 && d ==! 0) {
-  console.log(`O valor de d é ${d} e ele é um ńumero positivo`);
-} else {
-  console.log(`O valor de d é ${d} e ele é um ńumero negativo ou zero`);
-}
-
-let ar1 = Math.floor(Math.random()*90);
-let ar2 = Math.floor(Math.random()*90);
-let ar3 = Math.floor(Math.random()*90);
-
-
-console.log(ar1)
-console.log(ar2)
-console.log(ar3)
-
-if (ar1+ar2+ar3 > 180) {
-  console.log('é um triângulo')
-} else {console.log('não é um triângulo')}
-
-arr = ['pawn', 'bishop', 'knight', 'rook', 'queen', 'king']
-peca = arr[Math.floor(Math.random()*6)]
-
-switch (peca){
-  case 'pawn':
-  console.log('Pawns can only move forward');
-  break;
-
-  case 'bishop':
-  console.log('Bishops can move any number of squares diagonally.');
-  break;
-
-  case 'knight':
-  console.log('Knights can move only in an L-shape');
-  break;
-
-  case 'rook':
-  console.log('Rooks can move any number of squares, up and down and side to side.');
-  break;
-
-  case 'queen':
-  console.log('Queens can move any number of squares along ranks, files and diagonals.');
-  break;
-
-  case 'king':
-  console.log('Kings can move one square at a time in any direction.');
-  break;
-
-  default: console.log(`não deu certo, o valor de peça foi: ${peca}`);
-}
-
-
-// let notaPorcentagem = Math.floor(Math.random()*101);
-// console.log(notaPorcentagem)
-
-// switch (notaPorcentagem) {
-//   case notaPorcentagem >= 90:
-//     console.log('maior que 90')
-//     break;
-//   case notaPorcentagem <= 90:
-//     console.log('menor que 90')
-//     break;
-//   default:
-//     console.log('deu ruim')
+// if (a > b) {
+//   console.log('O maior valor é a')
+// } else {
+//   console.log('O maior valor é b')
 // }
 
+// if (a > b && a > c) {
+//   console.log('O maior número é o a')
+// } else if (b > a && b > c) {
+//   console.log('O maior ńumero é o b')
+// } else if (c > a && c > b) {
+//   console.log('O maior número é o c')
+// } else {console.log('Mais de um dos números é igual')}
 
-let num1 = 1;
-let num2 = 4;
-let num3 = 17;
+// if (d % 2 === 0 && d ==! 0) {
+//   console.log(`O valor de d é ${d} e ele é um ńumero positivo`);
+// } else {
+//   console.log(`O valor de d é ${d} e ele é um ńumero negativo ou zero`);
+// }
 
-for (i=1; i<3; i++){
-  if (num[i] % 2 === 0)
-  return true;
-  else {
-    return false;
-  }
+// let ar1 = Math.floor(Math.random()*90);
+// let ar2 = Math.floor(Math.random()*90);
+// let ar3 = Math.floor(Math.random()*90);
+
+
+// console.log(ar1)
+// console.log(ar2)
+// console.log(ar3)
+
+// if (ar1+ar2+ar3 > 180) {
+//   console.log('é um triângulo')
+// } else {console.log('não é um triângulo')}
+
+// arr = ['pawn', 'bishop', 'knight', 'rook', 'queen', 'king']
+// peca = arr[Math.floor(Math.random()*6)]
+
+// switch (peca){
+//   case 'pawn':
+//   console.log('Pawns can only move forward');
+//   break;
+
+//   case 'bishop':
+//   console.log('Bishops can move any number of squares diagonally.');
+//   break;
+
+//   case 'knight':
+//   console.log('Knights can move only in an L-shape');
+//   break;
+
+//   case 'rook':
+//   console.log('Rooks can move any number of squares, up and dowconst a = 10');
+//   const b = 5;
+//   const c = 3;
+//   const d = Math.floor(Math.random()*11);
+  
+//   console.log('Soma: '+(a+b));
+//   console.log('Subtração: ' + (a-b));
+//   console.log('Multiplicação: ' + (a*b))
+//   console.log('Divisão: ' +(a/b))
+//   console.log('Modulo: ' + (a%b))
+  
+//   if (a > b) {
+//     console.log('O maior valor é a')
+//   } else {
+//     console.log('O maior valor é b')
+//   }
+  
+//   if (a > b && a > c) {
+//     console.log('O maior número é o a')
+//   } else if (b > a && b > c) {
+//     console.log('O maior ńumero é o b')
+//   } else if (c > a && c > b) {
+//     console.log('O maior número é o c')
+//   } else {console.log('Mais de um dos números é igual')}
+  
+//   if (d % 2 === 0 && d ==! 0) {
+//     console.log(`O valor de d é ${d} e ele é um ńumero positivo`);
+//   } else {
+//     console.log(`O valor de d é ${d} e ele é um ńumero negativo ou zero`);
+//   }
+  
+//   let ar1 = Math.floor(Math.random()*90);
+//   let ar2 = Math.floor(Math.random()*90);
+//   let ar3 = Math.floor(Math.random()*90);
+  
+  
+//   console.log(ar1)
+//   console.log(ar2)
+//   console.log(ar3)
+  
+//   if (ar1+ar2+ar3 > 180) {
+//     console.log('é um triângulo')
+//   } else {console.log('não é um triângulo')}
+  
+//   arr = ['pawn', 'bishop', 'knight', 'rook', 'queen', 'king']
+//   peca = arr[Math.floor(Math.random()*6)]
+  
+//   switch (peca){
+//     case 'pawn':
+//     console.log('Pawns can only move forward');
+//     break;
+  
+//     case 'bishop':
+//     console.log('Bishops can move any number of squares diagonally.');
+//     break;
+  
+//     case 'knight':
+//     console.log('Knights can move only in an L-shape');
+//     break;
+  
+//     case 'rook':
+//     console.log('Rooks can move any number of squares, up and down and side to side.');
+//     break;
+  
+//     case 'queen':
+//     console.log('Queens can move any number of squares along ranks, files and diagonals.');
+//     break;
+  
+//     case 'king':
+//     console.log('Kings can move one square at a time in any direction.');
+//     break;
+  
+//     default: console.log(`não deu certo, o valor de peça foi: ${peca}`);
+//   }
+  
+//   let nota = Math.floor(Math.random()*101)
+//   console.log(nota)
+  
+//   switch (true) {
+//     case nota >= 90:
+//         console.log("A");
+//         break;
+//     case nota >= 80:
+//         console.log("B");
+//         break;
+//     case nota >= 70:
+//         console.log("C");
+//         break;
+//     case nota >= 60:aliquotaIR
+//         console.log("D");
+//         break;
+//     case nota >= 50:
+//         console.log("E");
+//         break;
+//     case nota < 50:
+//         console.log("F");
+//         break;
+//     default:
+//         console.log("Deu errado");
+//   }
+  
+//   let el1 = Math.floor(Math.random()*11);
+//   let el2 = Math.floor(Math.random()*11);
+//   let el3 = Math.floor(Math.random()*11);
+  
+//   console.log(el1);
+//   console.log(el2);
+//   console.log(el3);
+  
+//   if (el1 % 2 === 0){
+//     console.log(true);
+//   } else if (el2 % 2 === 0){
+//     console.log(true);
+//   } else if (el3 % 2 === 0){
+//     console.log(true);
+//   } else {console.log('Nenhum é par')}
+  
+//   let custo = Math.floor(Math.random()*11)
+//   let venda = Math.floor(Math.random()*21)
+//   let custoTotal = custo + (custo * .20)
+//   let lucro = venda - custoTotal
+  
+//   console.log(custo)
+//   console.log(venda)
+//   console.log(custoTotal)
+  
+  
+//   if (lucro > 0){
+//     console.log(`Meu lucro foi de ${lucro}`)
+//   } else if (lucro <= 0){
+//     console.log('deu ruim o código ou a venda')
+//   }
+  
+//   console.log('Queens can move any number of squares along ranks, files and diagonals.');
+//   break;
+
+//   case 'king':
+//   console.log('Kings can move one square at a time in any direction.');
+//   break;
+
+//   default: console.log(`não deu certo, o valor de peça foi: ${peca}`);
+// }
+
+// let nota = Math.floor(Math.random()*101)
+// console.log(nota)
+
+// switch (true) {
+//   case nota >= 90:
+//       console.log("A");
+//       break;
+//   case nota >= 80:
+//       console.log("B");
+//       break;
+//   case nota >= 70:
+//       console.log("C");
+//       break;
+//   case nota >= 60:
+//       console.log("D");
+//       break;
+//   case nota >= 50:
+//       console.log("E");
+//       break;
+//   case nota < 50:
+//       console.log("F");
+//       break;
+//   default:
+//       console.log("Deu errado");
+// }
+
+// let el1 = Math.floor(Math.random()*11);
+// let el2 = Math.floor(Math.random()*11);
+// let el3 = Math.floor(Math.random()*11);
+
+// console.log(el1);
+// console.log(el2);
+// console.log(el3);
+
+// if (el1 % 2 === 0){
+//   console.log(true);
+// } else if (el2 % 2 === 0){
+//   console.log(true);
+// } else if (el3 % 2 === 0){
+//   console.log(true);
+// } else {console.log('Nenhum é par')}
+
+// let custo = Math.floor(Math.random()*11)
+// let venda = Math.floor(Math.random()*21)
+// let custoTotal = custo + (custo * .20)
+// let lucro = venda - custoTotal
+
+// console.log(custo)
+// console.log(venda)
+// console.log(custoTotal)
+
+
+// if (lucro > 0){
+//   console.log(`Meu lucro foi de ${lucro}`)
+// } else if (lucro <= 0){
+//   console.log('deu ruim o código ou a venda')
+// }
+
+let salarioBruto = 3000;
+let aliquota = 0;
+
+if (salarioBruto <= 1556.94){
+  aliquota = salarioBruto * 0.08;
+} else if (salarioBruto <= 2594.92){
+  aliquota = salarioBruto * 0.09;
+} else if (salarioBruto <= 5189.82){
+  aliquota = salarioBruto * 0.11;
+} else {
+  aliquota = 570.88;
 }
+
+let salarioBase = salarioBruto - aliquota;
+
+if (salarioBase <= 1903.98){
+  aliquotaIR = 0;
+} else if (salarioBase <= 2826.65){
+  aliquotaIR = (salarioBase * 0.075) - 142.80;  
+} else if (salarioBase <= 3751.05){
+  aliquotaIR = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase <= 4664.68){
+  aliquotaIR = (salarioBase * 0.225) - 636.13;
+} else {
+  aliquotaIR = (salarioBase * 0.275) - 869.36;
+}
+
+console.log(aliquota)
+console.log(aliquotaIR)
+console.log(salarioBase)
+
+console.log('Meu salário é de ' + (salarioBase - aliquotaIR))
+
