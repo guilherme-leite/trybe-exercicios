6c5383784831a854cad16fa2c70c4c581ba58cea
Guilherme Leite <guilhermelax@hotmail.com>
2021-10-15 10:26:16 -0300

    finalizando exercicios dia 3.2, faltam os bonus

---
+++ fundamentos/bloco-3-introducao-a-html-e-css/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/script.js
@@ -0,0 +1,66 @@
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i])
+// }
+
+// let soma = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//     soma += numbers[i]
+// }
+// console.log()
+// console.log('O valor da soma dos valores é de ' + soma + '.', 'A média aritmética é de ' + (soma / numbers.length) + '.')
+
+// if (soma/numbers.length > 20){
+//     console.log('Valor maior que 20')
+// }   else {
+//     console.log('Valor menor ou igual a 20')
+// }
+
+// let maiorNumero = [];
+
+// for (let i = 0; i < numbers.length; i++){
+//     if (numbers[i] > maiorNumero){
+//         maiorNumero = numbers[i]    
+//     }
+// }
+
+// console.log('O maior número no array é ' + maiorNumero + '.')
+
+// let contadorOdds = 0;
+
+// for (let i = 0; i < numbers.length; i++){
+//     if (numbers[i] % 2 === 1){
+//       contadorOdds += 1
+//       console.log(numbers[i])
+//     } if (contadorOdds === 0){
+//       console.log('nenhum valor ímpar')
+//     }
+// }
+
+// console.log(contadorOdds);
+
+
+// let contadorMenor = numbers[0];
+
+// for (let i = 0; i < numbers.length; i++){
+//   if (contadorMenor > numbers[i]){
+//     contadorMenor = numbers[i];
+//   }
+// }
+
+// console.log(contadorMenor);
+
+// let newArr = [];
+
+// for(i=1; i <= 25; i++){
+//   newArr.push(i);
+// }
+
+// console.log('-----------')
+
+// for(i=0; i < newArr.length; i++){
+//   console.log(newArr[i]*2)
+// }
+
