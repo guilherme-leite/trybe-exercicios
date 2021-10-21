4b7a1486cf359bb8e5054838be65fffb5c092b06
Guilherme Leite <guilhermelax@hotmail.com>
2021-10-21 10:21:27 -0300

    adicionando index scrip e stytle para fazer exercícios do bloco 5

---
+++ fundamentos/bloco-3-introducao-a-html-e-css/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,5 @@
+document.getElementById('page-title').innerText = 'This is the Matrix babe';
+document.getElementById('paragraph').innerText = 'Um belo filme que assisti 1000 vezes';
+document.getElementById('subtitle').innerText = 'Woe'
+document.getElementById('second-paragraph').innerText = 'Acho que dificilmente o 4o será bom.'
+
