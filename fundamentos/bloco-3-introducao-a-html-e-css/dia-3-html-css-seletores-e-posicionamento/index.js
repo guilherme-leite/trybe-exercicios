d7ea61237eb4c1a1c18b2823a5ba63b9d496ef49
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-16 15:46:52 -0300

    Alteraçoes

--- fundamentos/bloco-3-introducao-a-html-e-css/dia-3-html-css-seletores-e-posicionamento/index.js
+++ fundamentos/bloco-3-introducao-a-html-e-css/dia-3-html-css-seletores-e-posicionamento/index.js
@@ -1,73 +1,3 @@
-// Parte I - Objetos e For/In
-
-// Usando o objeto abaixo, faça os exercícios a seguir:
-
-// let info = {
-//   personagem: 'Margarida',
-//   origem: 'Pato Donald',
-//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-// };
-
-// 1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
-
-// Bem-vinda, Margarida
-
-// 2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
-
-// {
-//   personagem: 'Margarida',
-//   origem: 'Pato Donald',
-//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-//   recorrente: 'Sim'
-// }
-
-// 3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
-
-// personagem
-// origem
-// nota
-// recorrente
-
-// 4 - Faça um novo for/in , mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
-
-// Margarida
-// Pato Donald
-// Namorada do personagem principal nos quadrinhos do Pato Donald
-// Sim
-
-// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
-
-// Margarida e Tio Patinhas
-// Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
-// Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
-// Ambos recorrentes // Atenção para essa última linha!
-// Usando o objeto abaixo, faça os exercícios a seguir:
-
-// let leitor = {
-//   nome: 'Julia',
-//   sobrenome: 'Pessoa',
-//   idade: 21,
-//   livrosFavoritos: [
-//     {
-//       titulo: 'O Pior Dia de Todos',
-//       autor: 'Daniela Kopsch',
-//       editora: 'Tordesilhas',
-//     },
-//   ],
-// };
-
-// 6 - Acesse as chaves nome , sobrenome e titulo , que está dentro da chave livrosFavoritos , e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
-
-// 7 - Adicione um novo livro favorito na chave livrosFavoritos , que é um array . Atribua a esta chave um objeto contendo as seguintes informações:
-
-// {
-//   titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-//   autor: 'JK Rowling',
-//   editora: 'Rocco',
-// }
-
-// 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
-
 let info = {
   personagem: 'Margarida',
   origem: 'Pato Donald',
@@ -118,32 +48,6 @@ function returnAllValuesFromBothObjects() {
   }
 
 }
-
-function romanNumCalculator(num) {
-  const romanNums = {
-    i: 1,
-    v: 5,
-    x: 10,
-    l: 50,
-    c: 100,
-    d: 500,
-    m: 1000,  
-  }
-  let total = 0;
-  let numeral = num.split('');
-  for (let i = 0; i < numeral.length; i++){
-    if(numeral[i] > numeral[i+1] || numeral[i] === numeral[i+1]){
-      total = romanNums[numeral[i]] + romanNums[numeral[i+1]];
-      console.log(total);
-    } else {
-      total = romanNums[numeral[i+1]] - romanNums[numeral[i]];
-      console.log(total);
-    }
-  }
-  return total;
-}
-
-console.log(romanNumCalculator('ii'));
 
 returnAllValuesFromBothObjects();
 
@@ -272,27 +176,3 @@ function checksWordEnd(strWord, strEnd) {
 }
 
 console.log(checksWordEnd('trybe', 'ba'));
-
-function romanNumCalculator(num) {
-  const romanNums = {
-    i: 1,
-    v: 5,
-    x: 10,
-    l: 50,
-    c: 100,
-    d: 500,
-    m: 1000,  
-  }
-  let total = 0;
-  let numeral = num.split('');
-  for (let i = 0; i < numeral.length; i++){
-    if(numeral[i] > numeral[i+1] || numeral[i] === numeral[i+1]){
-      return total = romanNums[numeral[i]] + romanNums[numeral[i+1]];
-    } else {
-      return total = romanNums[numeral[i+1]] - romanNums[numeral[i]];
-    }
-  }
-  return total;
-}
-
-console.log(romanNumCalculator('xl'));
