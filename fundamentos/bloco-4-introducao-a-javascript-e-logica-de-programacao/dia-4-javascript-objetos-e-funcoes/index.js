d388a98d2dd7da9101860bcc61429f91508033b7
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-16 14:43:15 -0300

    Feat: Bonus 1, calculadora de numerais romanos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/index.js
@@ -68,181 +68,231 @@
 
 // 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
 
-let info = {
-  personagem: 'Margarida',
-  origem: 'Pato Donald',
-  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  recorrente: 'Sim',
-};
+// let info = {
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim',
+// };
 
-function greeter(key) {
-  return 'Olá ' + info[key]
-}
+// function greeter(key) {
+//   return 'Olá ' + info[key]
+// }
 
-console.log(greeter('personagem'));
+// console.log(greeter('personagem'));
 
-function isReocurring(key) {
-  console.log(info[key]);
-}
+// function isReocurring(key) {
+//   console.log(info[key]);
+// }
 
-isReocurring('recorrente');
+// isReocurring('recorrente');
 
-function returnAllKeys() {
-  for (let key in info) {
-    console.log(key);
-  }
-}
+// function returnAllKeys() {
+//   for (let key in info) {
+//     console.log(key);
+//   }
+// }
 
-returnAllKeys();
+// returnAllKeys();
 
-function returnAllValues() {
-  for (let values in info) {
-    console.log(info[values]);
-  }
-}
+// function returnAllValues() {
+//   for (let values in info) {
+//     console.log(info[values]);
+//   }
+// }
 
-returnAllValues();
+// returnAllValues();
 
-let info2 = {
-  personagem: 'Tio Patinhas', 
-  origem: 'Christmas on Bear Mountain Dells Four Color Comics #178', 
-  nota:'O último MacPatinhas', 
-  recorrente: 'Sim',
-};
+// let info2 = {
+//   personagem: 'Tio Patinhas', 
+//   origem: 'Christmas on Bear Mountain Dells Four Color Comics #178', 
+//   nota:'O último MacPatinhas', 
+//   recorrente: 'Sim',
+// };
 
-function returnAllValuesFromBothObjects() {
-  console.log(info['personagem'] + ' e ' + info2['personagem']);
-  console.log(info['origem'] + ' e ' + info2['origem']);
-  if (info['recorrente'] && info2['recorrente'] === 'Sim') {
-    console.log('Ambos recorrentes');
-  }
+// function returnAllValuesFromBothObjects() {
+//   console.log(info['personagem'] + ' e ' + info2['personagem']);
+//   console.log(info['origem'] + ' e ' + info2['origem']);
+//   if (info['recorrente'] && info2['recorrente'] === 'Sim') {
+//     console.log('Ambos recorrentes');
+//   }
 
-}
+// }
 
-returnAllValuesFromBothObjects();
+// function romanNumCalculator(num) {
+//   const romanNums = {
+//     i: 1,
+//     v: 5,
+//     x: 10,
+//     l: 50,
+//     c: 100,
+//     d: 500,
+//     m: 1000,  
+//   }
+//   let total = 0;
+//   let numeral = num.split('');
+//   for (let i = 0; i < numeral.length; i++){
+//     if(numeral[i] > numeral[i+1] || numeral[i] === numeral[i+1]){
+//       total = romanNums[numeral[i]] + romanNums[numeral[i+1]];
+//       console.log(total);
+//     } else {
+//       total = romanNums[numeral[i+1]] - romanNums[numeral[i]];
+//       console.log(total);
+//     }
+//   }
+//   return total;
+// }
 
-let leitor = {
-  nome: 'Julia',
-  sobrenome: 'Pessoa',
-  idade: 21,
-  livrosFavoritos: [
-    {
-      titulo: 'O Pior Dia de Todos',
-      autor: 'Daniela Kopsch',
-      editora: 'Tordesilhas',
-    },
-    {
-      titulo: 'Harry Potter e o Prisioneiro de Azkaban',
-      autor: 'JK Rowling',
-      editora: 'Rocco',
-    },
-  ],
-};
+// console.log(romanNumCalculator('ii'));
 
-function phrase() {
-  console.log('O livro favorito de ' + leitor['nome'] + ' ' + leitor['sobrenome'] + ' se chama ' + '"' + leitor['livrosFavoritos'][0]['titulo'] + '"');
-}
+// returnAllValuesFromBothObjects();
 
-phrase();
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//     {
+//       titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//       autor: 'JK Rowling',
+//       editora: 'Rocco',
+//     },
+//   ],
+// };
 
-function favoriteBookCount() {
-  console.log(leitor['nome'] + ' tem ' + leitor['livrosFavoritos'].length + ' livros favoritos');
-}
+// function phrase() {
+//   console.log('O livro favorito de ' + leitor['nome'] + ' ' + leitor['sobrenome'] + ' se chama ' + '"' + leitor['livrosFavoritos'][0]['titulo'] + '"');
+// }
 
-favoriteBookCount();
+// phrase();
 
-function isPallindrome(str) {
-  let strArr = str.split('');
-  console.log(strArr);
-  let reverse = strArr.reverse().join('');
-  console.log(reverse);
-  if(str === reverse) {
-    return true;
-  } return false;
+// function favoriteBookCount() {
+//   console.log(leitor['nome'] + ' tem ' + leitor['livrosFavoritos'].length + ' livros favoritos');
+// }
+
+// favoriteBookCount();
+
+// function isPallindrome(str) {
+//   let strArr = str.split('');
+//   console.log(strArr);
+//   let reverse = strArr.reverse().join('');
+//   console.log(reverse);
+//   if(str === reverse) {
+//     return true;
+//   } return false;
   
-};
+// };
 
-console.log(isPallindrome('arara'));
-console.log(isPallindrome('paralelepipedo'));
+// console.log(isPallindrome('arara'));
+// console.log(isPallindrome('paralelepipedo'));
 
-function highestValueIndex(arr) {
-  let highest = 0;
-  for (let index in arr) {
-    if (arr[highest] < arr[index]) {
-      highest = index;
+// function highestValueIndex(arr) {
+//   let highest = 0;
+//   for (let index in arr) {
+//     if (arr[highest] < arr[index]) {
+//       highest = index;
+//     }
+//   }
+//   return highest;
+// }
+// console.log(highestValueIndex([2, 3, 6, 7, 10, 1]));
+
+// function lowestValueIndex(arr) {
+//   let lowest = 0;
+//   for (let index in arr) {
+//     if (arr[lowest] > arr[index]) {
+//       lowest = index;
+//     }  
+//   }
+//   return lowest;
+// }
+
+// console.log(lowestValueIndex([2, 4, 6, 7, 10, 0, -3]));
+
+// function returnHighestLenghtString(arr) {
+//   let highest = 0;
+//   for (let index in arr) {
+//     if (arr[highest].length < arr[index].length){
+//       highest = index;
+//     }
+//   }
+//   return arr[highest];
+// }
+
+// console.log(returnHighestLenghtString(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+// function returnHighestLenghtString(arr) {
+//   let highest = 0;
+//   for (let index in arr) {
+//     if (arr[highest].length < arr[index].length){
+//       highest = index;
+//     }
+//   }
+//   return arr[highest];
+// }
+
+// console.log(returnHighestLenghtString(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
+
+// function returnMostOccuringNum(arr) {
+//   let occurences = 0;
+//   for (let index in arr) {
+//     if (arr[occurences] === arr[index]) {
+//       occurences + 1;
+//     }
+//   }
+//   return arr[occurences];
+// }
+
+// console.log(returnMostOccuringNum([2, 3, 2, 5, 8, 2, 3]));
+
+// function returnFactorial(num) {
+//   let sum = 0;
+//   for (let index = 0; index <= num; index += 1) {
+//     sum += index;
+//   }
+//   return sum;
+// }
+
+// console.log(returnFactorial(5));
+
+// function checksWordEnd(strWord, strEnd) {
+//   let startArr = strWord.split('');
+//   let endArr = strEnd.split('');
+//   for (let index = 0; index < endArr.length; index += 1){
+//     if (endArr[index] !== startArr[index + strEnd.length + 1]){
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(checksWordEnd('trybe', 'ba'));
+
+function romanNumCalculator(num) {
+  const romanNums = {
+    i: 1,
+    v: 5,
+    x: 10,
+    l: 50,
+    c: 100,
+    d: 500,
+    m: 1000,  
+  }
+  let total = 0;
+  let numeral = num.split('');
+  for (let i = 0; i < numeral.length; i++){
+    if(numeral[i] > numeral[i+1] || numeral[i] === numeral[i+1]){
+      return total = romanNums[numeral[i]] + romanNums[numeral[i+1]];
+    } else {
+      return total = romanNums[numeral[i+1]] - romanNums[numeral[i]];
     }
   }
-  return highest;
-}
-console.log(highestValueIndex([2, 3, 6, 7, 10, 1]));
-
-function lowestValueIndex(arr) {
-  let lowest = 0;
-  for (let index in arr) {
-    if (arr[lowest] > arr[index]) {
-      lowest = index;
-    }  
-  }
-  return lowest;
+  return total;
 }
 
-console.log(lowestValueIndex([2, 4, 6, 7, 10, 0, -3]));
-
-function returnHighestLenghtString(arr) {
-  let highest = 0;
-  for (let index in arr) {
-    if (arr[highest].length < arr[index].length){
-      highest = index;
-    }
-  }
-  return arr[highest];
-}
-
-console.log(returnHighestLenghtString(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
-
-function returnHighestLenghtString(arr) {
-  let highest = 0;
-  for (let index in arr) {
-    if (arr[highest].length < arr[index].length){
-      highest = index;
-    }
-  }
-  return arr[highest];
-}
-
-console.log(returnHighestLenghtString(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
-
-function returnMostOccuringNum(arr) {
-  let occurences = 0;
-  for (let index in arr) {
-    if (arr[occurences] === arr[index]) {
-      occurences + 1;
-    }
-  }
-  return arr[occurences];
-}
-
-console.log(returnMostOccuringNum([2, 3, 2, 5, 8, 2, 3]));
-
-function returnFactorial(num) {
-  let sum = 0;
-  for (let index = 0; index <= num; index += 1) {
-    sum += index;
-  }
-  return sum;
-}
-
-console.log(returnFactorial(5));
-
-function checksWordEnd(strWord, strEnd) {
-  let startArr = strWord.split('');
-  let endArr = strEnd.split('');
-  for (let index = 0; index < endArr.length; index += 1){
-    if (endArr[index] !== startArr[index + strEnd.length + 1]){
-      return false;
-    }
-  }
-  return true;
-}
-
-console.log(checksWordEnd('trybe', 'ba'));
+console.log(romanNumCalculator('xl'));
