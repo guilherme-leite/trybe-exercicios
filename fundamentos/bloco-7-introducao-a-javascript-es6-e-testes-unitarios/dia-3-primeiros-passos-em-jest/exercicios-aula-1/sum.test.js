ed5c2b40e48de2d5ceafce9ce528ab2751a0be9a
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-27 18:35:03 -0300

    Feat: Cobertura de testes de funcao sum

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios-aula-1/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios-aula-1/sum.test.js
@@ -8,6 +8,15 @@ const sum = require('./sum');
 
 describe("A função sum(a, b) retorna a soma do parâmetro a com o b", () => {
   test("Teste se o retorno de sum(4, 5) é 9",() => {
-    epect(sum(4,5).toBeEqual(9));
+    expect(sum(4,5)).toBe(9);
+  });
+
+  test("Teste se o retorno de sum(0,0) é 0", () => {
+    expect(sum(0,0)).toBe(0);
+  });
+
+  test("Teste se a funcao sum lanca um erro quando os parametros sao 4 e '5'", () => {
+    expect(() => sum(4, '5')).toThrowError();
+    expect(() => sum(4,'5')).toThrowError('parameters must be numbers');
   });
 });
