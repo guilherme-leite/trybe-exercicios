0831fc69da9506ae5a991cc8e24d21961713f669
Guilherme Leite <guilhermelax@hotmail.com>
2022-03-28 07:05:48 -0300

    Feat: Criando arquivo myRemove.js sua funcao e implementando seus testes na myRemove.test.js

---
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/exercicios-aula-1/myRemove.test.js
@@ -0,0 +1,15 @@
+const myRemove = require('./myRemove');
+
+describe("A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array", () => {
+  test("Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado",() => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1,2,4]);
+  });
+
+  test("Verifique se a chamada myRemove([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]",() => {
+    expect(myRemove([1,2,3,4], 3)).not.toEqual([1,2,3,4]);
+  });
+
+  test("verifique se a chamada myRemove([1,2,3,4], 5) retorna o array esperado", () => {
+    expect(myRemove([1,2,3,4], 5)).toEqual([1,2,3,4]);
+  });
+});
