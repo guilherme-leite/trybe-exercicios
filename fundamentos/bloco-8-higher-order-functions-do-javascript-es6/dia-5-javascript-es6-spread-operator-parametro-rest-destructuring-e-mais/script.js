6443e1360b679b4673728b2c49772b5af45a4a9a
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-03 19:05:56 -0300

    Feat: Exercicio 5 finalizado

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/script.js
@@ -94,4 +94,13 @@ console.log(swap(myList));
 
 //
 
+const palio = ['Palio', 'Fiat', 2019];
+const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
+const chiron = ['Chiron', 'Bugatti', 2016];
+
+const toObject = ([type, brand, year]) => ({type, brand, year}); 
+
+console.log(toObject(palio));
+
+//
 
