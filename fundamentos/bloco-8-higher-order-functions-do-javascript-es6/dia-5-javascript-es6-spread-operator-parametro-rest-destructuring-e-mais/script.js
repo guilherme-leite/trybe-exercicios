97d6b675e81c8f5102c0a2a81b54be7570b44f45
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-03 18:28:35 -0300

    Feat: Exercicio 2 finalizado

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/script.js
@@ -14,5 +14,16 @@ rectangles.forEach((rectangle) => {
 
 //
 
+const numbersArr = [1,2,3,4,5,6,7];
+
+function sum(...args) {
+  const result = args.reduce((acc, curr) => acc + curr);
+  console.log(result);
+}
+
+sum(...numbersArr);
+
+//
+
 
 
