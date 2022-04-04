2b749c5269be3b320696a30b53aa29a721056b6e
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-04 09:18:44 -0300

    Feat: Finalizando exercicio 3

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -66,3 +66,18 @@ setTimeout(() => console.log(planetDistanceFromSun(jupiter)), 2000); // C
 
 // A,C,B
 
+const getPlanet = () => {
+  const mars = {
+    name: "Mars",
+    distanceFromSun: {
+      value: 227900000,
+      measurementUnit: "kilometers",
+    },
+  };
+  setTimeout(() => console.log("Returned planet: ", mars), 4000);
+};
+
+getPlanet(); // imprime Marte depois de 4 segundos;
+
+//
+
