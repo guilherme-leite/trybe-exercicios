3701758a2e1b61d892d09ec9a286a97b19ca1be8
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-04 09:27:29 -0300

    Feat: Finalizando exercicio 4

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/script.js
@@ -81,3 +81,22 @@ getPlanet(); // imprime Marte depois de 4 segundos;
 
 //
 
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = () => {
+  const currentTemperature = getMarsTemperature();
+  setTimeout(() => {
+    console.log("Mars temperature is:", currentTemperature);
+  }, messageDelay());
+};
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
+
+//
+
