41574e1c3d8c94daacb40a6881d85d7c057afad9
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-07 11:02:31 -0300

    Feat: Exercicio de fixacão 3 finalizado

---
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/Exercicio de fixacao 3.sql
@@ -0,0 +1,6 @@
+SELECT * FROM sakila.city;
+SELECT city FROM sakila.city;
+SELECT first_name, last_name FROM sakila.customer;
+SELECT * FROM sakila.rental;
+SELECT title, description, release_year From sakila.film;
+SELECT * FROM sakila.actor;
