5c1f1d27607dfec80bf16831af8ca5e34f053df6
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-07 11:14:19 -0300

    Feat: Adicionando arquivos de exemplo de aula 1 e 2

---
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/Exemplo de aula 1.sql
@@ -0,0 +1,4 @@
+SELECT * FROM sakila.actor;
+SELECT CONCAT(first_name, last_name) FROM sakila.actor;
+SELECT CONCAT(first_name, ' ', last_name) FROM sakila.actor;
+SELECT CONCAT(first_name, ' ', last_name) AS 'Nome Completo' FROM sakila.actor;
---
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/Exemplo de aula 2.sql
@@ -0,0 +1,6 @@
+SELECT * FROM sakila.film;
+SELECT title, release_year FROM sakila.film;
+SELECT CONCAT(title, ' ', rating) AS 'Classificão' FROM sakila.film;
+SELECT * FROM sakila.address;
+SELECT CONCAT(address,' ' , district) AS 'Endereco' FROM sakila.address;
+
