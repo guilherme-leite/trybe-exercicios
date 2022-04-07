f6d2431284c28a51f11662b65df4d77912346eb5
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-07 10:36:44 -0300

    Feat: Exercicio de fixacão 2 finalizado

---
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/Exercicio de fixacao 2.sql
@@ -0,0 +1,5 @@
+SELECT 'Guilherme';
+SELECT 'Guilherme', 'Leite', 'Nanuque', 33;
+SELECT 'Guilherme' AS nome, 'Leite' AS sobrenome, 'Nanuque' AS 'cidade natal', 33 AS idade;
+SELECT 13 * 8;
+SELECT now() AS 'Data Atual';
