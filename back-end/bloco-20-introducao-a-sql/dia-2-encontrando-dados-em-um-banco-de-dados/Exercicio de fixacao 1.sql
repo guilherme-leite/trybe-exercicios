0b73d81073ccc09b77d9cbdae3b0e61bab788ccb
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-07 10:29:13 -0300

    Feat: Estrutura de arquivos do dia e primeiro arquivo de exercicio de fixacao

---
+++ back-end/bloco-20-introducao-a-sql/dia-2-encontrando-dados-em-um-banco-de-dados/Exercicio de fixacao 1.sql
@@ -0,0 +1,9 @@
+SELECT 'Olá, bem-vindo ao SQL!';
+SELECT 10;
+SELECT now();
+SELECT 20*2;
+SELECT 50 / 2;
+SELECT 18 AS idade;
+SELECT 2019 AS ano;
+SELECT 'Rafael', 'Martins', 25, 'Desenvolvedor Web';
+SELECT 'Rafael' AS nome, 'Martins' AS sobrenome, 25 AS idade, 'Desenvolvedor Web' AS 'Área de atuacão';
