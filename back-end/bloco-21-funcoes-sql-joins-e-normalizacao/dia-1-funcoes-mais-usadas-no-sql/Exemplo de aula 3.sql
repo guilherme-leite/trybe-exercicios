93c6f0dca80075e74f94040189767870574abf1a
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-10 19:38:36 -0300

    Feat: Finalizando novo exemplo de aula 3

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Exemplo de aula 3.sql
@@ -0,0 +1,13 @@
+SELECT IF(15 MOD 2 = 0, 'Par', 'Impar') AS 'Par ou ímpar';
+
+SELECT 220 DIV 12 AS 'Coporta';
+
+SELECT IF(220 MOD 12, 'Não há lugares sobrando', CONCAT('Sim, há ', 220 MOD 12, ' sobrando.')) AS 'Comporta?';
+
+SELECT ROUND(15 +(RAND() * 5));
+
+SELECT ROUND(15.7515971,5);
+
+SELECT FLOOR(39.494);
+
+SELECT CEIL(85.234);
