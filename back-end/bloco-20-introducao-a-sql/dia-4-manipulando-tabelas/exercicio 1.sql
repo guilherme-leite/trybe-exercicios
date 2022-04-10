6d36fc5f66be018fb1900d43981ed671d82a32ad
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-10 09:28:02 -0300

    Feat: Finalizando exercicios do dia

--- back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercicio 1.sql
+++ back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercicio 1.sql
@@ -6,3 +6,33 @@ VALUES
     ('Os Incríveis', 'Brad Bird', 2004, 116),
     ('WALL-E', 'Pete Docter', 2008, 104);
 SELECT title FROM `Movies`;
+SELECT * FROM BoxOffice;-- 
+INSERT INTO BoxOffice 
+	(movie_id,rating, domestic_sales, international_sales)
+VALUES
+	(8, 6.8, 45000000, 370000000);
+SELECT * FROM Movies;
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Movies
+SET director = 'Andrew Staton'
+WHERE title = 'Procurando Nemo';
+UPDATE Movies
+SET title = 'Ratatouille', year = 2007
+WHERE title = 'ratatui';
+INSERT INTO BoxOffice 
+	(movie_id,rating, domestic_sales, international_sales)
+VALUES
+	(8, 8.5, 300000000, 250000000),
+    (10, 7.4, 460000000, 510000000),
+    (11, 9.9, 290000000, 280000000);
+SELECT * FROM Movies;
+DELETE FROM BoxOffice
+WHERE movie_id = 11;
+DELETE FROM Movies
+WHERE title = 'WALL-E';
+DELETE FROM BoxOffice
+WHERE movie_id = 2 AND 9;
+DELETE FROM Movies
+WHERE director = 'Andrew Staton';
+
+
