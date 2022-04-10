ad64d2e1e420b8bc55b8a1dbfe02c2191c7246f0
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-10 18:35:47 -0300

    Feat: Estrutura de pastas e primeiro exemplo de aula

---
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Exemplo de aula 1.sql
@@ -0,0 +1,8 @@
+SELECT UCASE('meu nome é Gui');
+SELECT LCASE('meu nome é Gui');
+SELECT REPLACE('meu nome é Gui', 'Gui', 'Luana');
+SELECT LEFT('Oi meu nome é Gui', 5);
+SELECT RIGHT('Oi meu nome é Gui', 5);
+SELECT CHAR_LENGTH('Oi meu nome é Gui');
+SELECT SUBSTRING('Oi meu nome é Gui',5,5);
+SELECT SUBSTRING('Oi meu nome é Gui',5);
