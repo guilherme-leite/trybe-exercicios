7e80e5ef50d8a83d2d96445953c28bbe5f0270eb
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-11 08:31:22 -0300

    Feat: Finalizando exemplos de aula

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Exemplo de aula 6.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/Exemplo de aula 6.sql
@@ -40,3 +40,33 @@ SELECT first_name, COUNT(*)
 FROM actor
 GROUP BY first_name
 HAVING COUNT(*) > 2;
+
+SELECT first_name, COUNT(*) AS nomes_cadastrados
+FROM actor
+GROUP BY first_name
+HAVING nomes_cadastrados > 2;
+
+-- Para Fixar
+-- Usando a query a seguir, exiba apenas as durações médias que estão entre 115.0 a 121.50. Além disso, dê um alias (apelido) à coluna gerada por AVG(length) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma decrescente.
+
+--     SELECT rating, AVG(length)
+--     FROM sakila.film
+--     GROUP BY rating;
+
+SELECT rating, AVG(length) AS media_de_duracao
+FROM sakila.film
+GROUP BY rating
+HAVING media_de_duracao BETWEEN 115.0 AND 121
+ORDER BY  media_de_duracao DESC;
+
+-- Usando a query a seguir, exiba apenas os valores de total do custo de substituição que estão acima de $3950.50. Dê um alias que faça sentido para SUM(replacement_cost) , de forma que deixe a query mais legível. Finalize ordenando os resultados de forma crescente.
+-- Copiar
+--     SELECT rating, SUM(replacement_cost)
+--     FROM sakila.film
+--     GROUP by rating;
+
+SELECT rating, SUM(replacement_cost) AS custo_de_substituicao_total
+FROM sakila.film
+GROUP by rating
+HAVING custo_de_substituicao_total > 3950.50
+ORDER BY custo_de_substituicao_total ASC;
