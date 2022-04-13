face1a3fe29de4b8214b6d5515cb60615b943ddf
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-13 08:06:45 -0300

    Feat: Finalizando exercicios de aula 1

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/Exercicio de aula 1.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/Exercicio de aula 1.sql
@@ -23,14 +23,51 @@ INNER JOIN BoxOffice AS BO
 
 -- Exercício 2: Utilizando o INNER JOIN , faça uma busca que retorne o número de vendas para cada filme que possui um número maior de vendas internacionais ( international_sales ) do que vendas nacionais ( domestic_sales ).
 
-
+SELECT
+    M.title,
+    BO.domestic_sales + BO.international_sales AS `Vendas totais`
+FROM Movies AS M
+INNER JOIN BoxOffice AS BO
+ON BO.movie_id = M.id
+WHERE BO.international_sales > BO.domestic_sales;
 
 -- Exercício 3: Utilizando o INNER JOIN , faça uma busca que retorne os filmes e sua avaliação ( rating ) em ordem decrescente.
 
-
+SELECT
+    M.title,
+    BO.rating
+FROM Movies AS M
+INNER JOIN BoxOffice AS BO
+ON BO.movie_id = M.id
+ORDER BY BO.rating DESC;
 
 -- Exercício 4: Utilizando o LEFT JOIN , faça uma busca que retorne todos os dados dos cinemas, mesmo os que não possuem filmes em cartaz e, adicionalmente, os dados dos filmes que estão em cartaz nestes cinemas. Retorne os nomes dos cinemas em ordem alfabética.
 
+SELECT
+    T.name,
+    T.location,
+    M.title,
+    M.director,
+    M.year,
+    M.length_minutes,
+    M.theater_id
+FROM Theater AS T
+LEFT JOIN Movies AS M
+ON T.id = M.id
+ORDER BY T.name ASC;
 
 
 -- Exercício 5: Utilizando o RIGHT JOIN , faça uma busca que retorne todos os dados dos filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos cinemas que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em ordem alfabética.
+
+SELECT
+    M.title,
+    M.director,
+    M.year,
+    M.length_minutes,
+    M.theater_id,
+    T.name,
+    T.location
+FROM Movies AS M
+RIGHT JOIN Theater AS T
+ON M.id = T.id
+ORDER BY T.name ASC;
