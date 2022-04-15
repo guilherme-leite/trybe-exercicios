c6c52f674a86d2bb0ca957875aa787e40eeafeae
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-15 17:48:10 -0300

    Feat: Instalando readline e importando. Alterando o hello world para utilizar as novas funcionalidades importadas

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/index.js
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+const readline = require('readline-sync');
+
+const name = readline.question('Qual é o seu nome? ');
+const age = readline.questionInt('Qual sua idade? ');
+
+console.log(`Hello, ${name}! You are ${age} years old!`);
+// console.log('Hello, world!');
