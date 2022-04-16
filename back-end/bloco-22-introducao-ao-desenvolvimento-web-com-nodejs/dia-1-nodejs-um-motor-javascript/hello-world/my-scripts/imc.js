6319b6ef435ce1da3f29ee5c7afd28ccc3b8c1c5
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-16 06:32:57 -0300

    Feat: Finalizando o primeiro script

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/my-scripts/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/hello-world/my-scripts/imc.js
@@ -8,10 +8,23 @@ const altura = questionInt('Qual a sua altura? ');
 
 function calcImc() {
   console.log(`Peso: ${peso}, Altura: ${altura}`);
-
   const imc = (peso/ Math.pow(altura/ 100,2)).toFixed(2);
-
-  console.log(`IMC: ${imc}`);
+  console.log(`Seu IMC é: ${imc}`);
+  switch(imc) {
+    case imc < 18:
+      console.log('Abaixo do peso (magreza)');
+      break;
+    case imc > 18.5 && imc < 24.9:
+      console.log('Peso normal');
+    case imc > 25 && imc < 29.9:
+      console.log('Acima do peso (sobrepeso)');
+    case imc > 30 && imc < 34.9:
+      console.log('Obesidade graus I');
+    case imc > 35 && imc < 39.9:
+      console.log('Obesidade graus II');
+    default:
+      console.log('Obesidade graus III e IV');
+  }
 }
 
 calcImc();
