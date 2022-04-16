00a3c25dcc5dcb649979893285dd93e12561e8cb
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-16 07:51:36 -0300

    Finalizando: exercicio 8

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/index.js
@@ -1,18 +1,28 @@
 const { question } = require('readline-sync');
 
+const scrips = [
+  { name: 'Calcular IMC', script: './imc.js' },
+  { name: 'Calcular velocidade média', script: './velocidade.js' },
+  { name: 'Jogo de adivinhação', script: './sorteio.js' },
+];
+
 function chooseScript() {
-  console.log('1- imc');
-  console.log('2- velocidade');
-  console.log('3- sorteio');
+  let mensagem = scrips.map((script,index) => `${index + 1} ${script.name}`)
+  
+  mensagem.unshift('Escolha qual dos scripts você quer executar');
+
+  mensagem = mensagem.join('\n');
+
+  console.log(mensagem);
 
   const scrip = question('Qual script deseja rodar? ');
 
   if(script = 1) {
-    calcImc();
+    require('./imc.js');
   } else if (scrip = 2) {
-    calcAverageSpeed();
+    require('./velocidade.js');
   } else if (scrip = 3) {
-    generateDraw();
+    require('./sorteio.js');
   } else {
     console.log('Essa opcão não existe, tente novamente.');
     chooseScript();
