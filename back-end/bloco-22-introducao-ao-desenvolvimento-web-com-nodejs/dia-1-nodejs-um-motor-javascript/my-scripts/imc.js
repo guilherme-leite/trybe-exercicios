1d17c97b564a0567b899bd8fc302b092a16d4f8f
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-16 08:00:08 -0300

    Fix: Alterando switch case para ifs

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/my-scripts/imc.js
@@ -10,21 +10,33 @@ function calcImc() {
   console.log(`Peso: ${peso}, Altura: ${altura}`);
   const imc = (peso/ Math.pow(altura/ 100,2)).toFixed(2);
   console.log(`Seu IMC é: ${imc}`);
-  switch(imc) {
-    case imc < 18:
-      console.log('Abaixo do peso (magreza)');
-      break;
-    case imc > 18.5 && imc < 24.9:
-      console.log('Peso normal');
-    case imc > 25 && imc < 29.9:
-      console.log('Acima do peso (sobrepeso)');
-    case imc > 30 && imc < 34.9:
-      console.log('Obesidade graus I');
-    case imc > 35 && imc < 39.9:
-      console.log('Obesidade graus II');
-    default:
-      console.log('Obesidade graus III e IV');
+  
+  if (imc < 18.5) {
+    console.log('Situação: Abaixo do peso (magreza)');
+    return;
   }
+
+  if (imc >= 18.5 && imc < 25) {
+    console.log('Situação: Peso normal');
+    return;
+  }
+
+  if (imc >= 25 && imc < 30) {
+    console.log('Situação: Acima do peso (sobrepeso)');
+    return;
+  }
+
+  if (imc >= 30 && imc < 35) {
+    console.log('Situação: Obesidade grau I');
+    return;
+  }
+
+  if (imc >= 35 && imc < 40) {
+    console.log('Situação: Obesidade grau II');
+    return;
+  }
+
+  console.log('Situação: Obesidade graus III e IV');
 }
 
 calcImc();
