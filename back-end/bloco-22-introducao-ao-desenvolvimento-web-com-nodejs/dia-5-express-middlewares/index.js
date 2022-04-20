7fea0fff890b828d6ab02fb636b46702a9c91658
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-20 17:41:08 -0300

    Feat: Exercicio em grupo

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const userNameMinLength = 3;
+const pwMinLenght = 4;
+const pwMaxLenght = 8;
+
+const app = express();
+app.use(express.json());
+
+app.post('/use/register', function (req, res) {
+  const { username, email, password } = req.body;
+  const isValid = email.includes("@") && email.includes(".com");
+  const isPwValid = password.length >= pwMinLenght && password.length <= pwMaxLenght;
+  
+  if(username.length <= userNameMinLength || !isValid || !isPwValid ) {
+    return res.status(400).json({ message: "invalid data" });
+  }
+
+  return res.status(201).json({ message: "user created" });
+
+});
+
+app.listen(3002, () => {
+  console.log('Rodante na porta 3002');
+})
