a07763c9d29cbc8a3eb6c32373f4a06fad6cf0d6
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-20 09:38:32 -0300

    Fix: Apagando index.js

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/recipies-exemple/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/recipies-exemple/index.js
@@ -4,7 +4,7 @@ const app = express();
 const recipes = [
   { id:1, name: 'Lasanha', price: 40.0, waitTime: 30 },
   { id:2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
-  { id:3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 }
+  { id:3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
 app.get('/recipes', function (req, res) {
