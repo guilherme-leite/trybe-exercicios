88d11d8c27d463df6fb06fa6a69bce747feb153e
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-21 07:49:31 -0300

    Feat: Criando estrutura para o exercicio do dia e iniciando

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicio-1/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.get('*', function (req, res){
+  res.status(200).json({ message: 'Acho que algo deu errado, caiu no coronga!'});
+});
+
+app.listen(3001, () => { 
+  console.log('Ouvindo na 3001, exercicio-1');
+});
