e4b0c82c4279c57d0413d064491d622b3389db61
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-21 07:51:27 -0300

    Feat: Implementando rota /ping

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicio-1/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicio-1/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const app = express();
 
+app.get('/ping', function (req, res) {
+  res.status(200).json({ message: 'pong'});
+});
+
 app.get('*', function (req, res){
   res.status(200).json({ message: 'Acho que algo deu errado, caiu no coronga!'});
 });
