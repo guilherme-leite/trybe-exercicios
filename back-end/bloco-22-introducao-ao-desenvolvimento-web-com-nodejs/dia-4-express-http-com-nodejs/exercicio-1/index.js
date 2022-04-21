17fb821bb6d7d3d55e7724be8a6a30ad1ac9cc80
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-21 08:24:55 -0300

    Feat: Criando rota put /users/name/age

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicio-1/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/exercicio-1/index.js
@@ -18,7 +18,12 @@ app.post('/greetings', (req, res) => {
   if( parseInt(age, 10) > 17) {
     return res.status(200).json({ message: `Hello, ${name}!`});
   } else return res.status(401).json({ message: 'Unauthorized'});
-})
+});
+
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+  return res.status(200).json({ message: `Seu nome é ${name} e você tem ${age} anos de idade.`});
+});
 
 app.get('*', function (req, res){
   res.status(200).json({ message: 'Acho que algo deu errado, caiu no coronga!'});
