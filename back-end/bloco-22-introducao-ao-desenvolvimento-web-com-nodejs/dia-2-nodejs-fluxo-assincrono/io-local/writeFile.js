557ca3dce21a2ddfa51c6f37c5e198ddda3bc2b2
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-22 07:51:09 -0300

    Feat: Refatorando a funcao de escrever arquivos para utilizar async/await

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/writeFile.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-local/writeFile.js
@@ -1,9 +1,22 @@
 const fs = require('fs').promises;
 
-fs.writeFile('./meu-arquivo.txt', 'Provolone é um gato mágico')
-  .then(() => {
-    console.log('Arquivo escrito com sucesso');
-  })
-  .catch((err) => {
-    console.error(`Erro ao escrever o arquivo: ${err.message}`);
-  })
+// Utilizando promisses
+
+// fs.writeFile('./meu-arquivo.txt', 'Provolone é um gato mágico')
+//   .then(() => {
+//     console.log('Arquivo escrito com sucesso');
+//   })
+//   .catch((err) => {
+//     console.error(`Erro ao escrever o arquivo: ${err.message}`);
+//   });
+
+async function main() {
+  try {
+    await fs.writeFile('./meu-arquivo.txt', 'Provolone é um gato mágico');
+    console.log('Arquivo escrito com sucesso!');
+  } catch (err) {
+    console.error(`Erro ao escrever o arquivo: ${err}`);
+  }
+};
+
+main();
