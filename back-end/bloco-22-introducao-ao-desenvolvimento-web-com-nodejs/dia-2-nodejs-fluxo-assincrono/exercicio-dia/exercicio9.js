a78f1d1b21c98507d46775777587e1e0d483d24e
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-23 09:04:35 -0300

    Feat: Exercicio9 finalizado. Implementando funcao que pede nome do arquivo e frase para que subtitua e salve novamente.

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio-dia/exercicio9.js
@@ -0,0 +1,42 @@
+const fs = require('fs').promises;
+const readline = require('readline');
+
+function question(message) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+  return new Promise((resolve) => {
+    rl.question(message, (answer) => {
+      rl.close();
+      resolve(answer);
+    });
+  });
+}
+
+async function start() {
+  const fileName = await question('Arquivo a ser lido: ');
+
+  const originalContent = await fs.readFile(fileName, 'utf8')
+  .catch(err => {
+    console.log(`Erro ao ler o arquivo ${err}`);
+    return false;
+  })
+
+  if(!originalContent) return;
+
+const oldWord = await question('Qual palavra deseja substituir? ');
+const newWord = await question('E qual palavra deve ficar em seu lugar? ');
+
+const newContent = originalContent.replace(new RegExp(oldWord, 'g'), newWord);
+
+console.log('Resultado da substituicão: ');
+console.log(newContent);
+
+const destinationPath = await question('Onde deseja salvar o resultado? ');
+await fs.writeFile(destinationPath, newContent);
+
+}
+
+start();
