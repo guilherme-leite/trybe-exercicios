153c9ccb67631acd925be8ffdf3ca3a307f2d926
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-24 07:42:00 -0300

    Feat: Exercicio10 criando funcao fizzbuzz que recebe um numero e devolve uma promise com o valor

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio-dia/exercicio10.js
@@ -0,0 +1,20 @@
+// Escreva uma função que receba um número inteiro maior que 0 e retorne uma Promise.
+// Se o número for múltiplo de 3, resolva a Promise com o valor "Fizz".
+// Se o número for múltiplo de 5, resolva a Promise com o valor "Buzz".
+// Se o número for múltiplo de 3 e 5, resolva a Promise com o valor "FizzBuzz".
+// Caso contrário, rejeite a Promise com o valor do número.
+
+function fizzBuzz(num) {
+  const promise = new Promise((resolve, reject) => {
+    if (num % 3 === 0 && num % 5 === 0) {
+      resolve(console.log('FizzBuzz'));
+    } else if (num % 3 === 0) {
+      resolve(console.log('Fizz'));
+    } else if (num % 5 === 0) {
+      resolve(console.log('Buzz'));
+    }
+    return promise;
+  });
+}
+
+fizzBuzz(15);
