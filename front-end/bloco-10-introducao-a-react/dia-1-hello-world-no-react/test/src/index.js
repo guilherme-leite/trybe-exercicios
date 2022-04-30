de5175d4b2d0ee3a32c5e3f408a8ad47e4dab186
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-30 08:58:13 -0300

    Criando funcao tick que gera o relogio que é atualizado segundo a segundo

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/test/src/index.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/test/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function tick() {
+  const element = (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>It's { new Date().toLocaleTimeString('pt', { hour12: true})}.</h2>
+    </div>
+  );
+  ReactDom.render(element, document.getElementById('root'));
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(tick, 1000);
