a5ccf845c619afe4d3b9978a75d36e40a38ec67b
Guilherme Leite <guilhermelax@hotmail.com>
2022-04-30 11:14:30 -0300

    Feat: Criando funcao task renderizando ela no app

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-do-dia-01/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-do-dia-01/src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
 
+const task = (value) => {
+  return (
+    <li>{ value }</li>
+  );
+}
+
 function App() {
   return (
-    //
+    <ol>
+      {task('Andar de bike')}
+    </ol>
     );
 }
 
