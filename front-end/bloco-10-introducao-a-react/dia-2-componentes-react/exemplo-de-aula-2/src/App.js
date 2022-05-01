3b36f554cedc940f1b0464e6aef8c604d6b138b5
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-01 10:19:35 -0300

    Criando array de lis com produtos a partir do array shoppingList

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exemplo-de-aula-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exemplo-de-aula-2/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
 
 function App() {
+  const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+  const items = shoppingList.map((item, index) => (
+    <li key={index}>{ item }</li>
+  ));
   return (
-    
-  );
+    <div>
+      <h2>Lista de compras</h2>
+      <ul>
+        { items }
+      </ul>
+    </div>
+    );
 }
 
 export default App;
