23126b4cae1e5080c1348b551606d561ab82f8a3
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-01 10:22:51 -0300

    Comentadno para fazer o proximo exemplo

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exemplo-de-aula-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exemplo-de-aula-2/src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
 
 function App() {
-  const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
-  const items = shoppingList.map((item, index) => (
-    <li key={index}>{ item }</li>
-  ));
+  // const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+  // const items = shoppingList.map((item, index) => (
+  //   <li key={index}>{ item }</li>
+  // ));
+  // return (
+  //   <div>
+  //     <h2>Lista de compras</h2>
+  //     <ul>
+  //       { items }
+  //     </ul>
+  //   </div>
+  //   );
   return (
-    <div>
-      <h2>Lista de compras</h2>
-      <ul>
-        { items }
-      </ul>
-    </div>
-    );
+    
+  )
 }
 
 export default App;
