27ce2decc9a940b0675c28c0f134ecc05fc548cb
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-01 18:05:51 -0300

    Feat: Importando json e Pokedex que leva a const que salva esse json como props para ela.

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio-de-aula-1/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio-de-aula-1/src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import pokemons from './data';
+import Pokedex from './Pokedex';
 
 function App() {
   return (
-    
+    <div className='App'>
+      <h1> Pokedex </h1>
+      <Pokedex pokemons={pokemons} />
+    </div>
   );
 }
 
