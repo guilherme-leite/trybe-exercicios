0c2f79f372b60a95819822cf7298e090a4edaf95
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-01 17:52:35 -0300

    Feat: Criando o componente Pokemon que vai criar cada cabo de pokemon.

---
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exercicio-de-aula-1/src/Pokemon.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+class Pokemon extends React.Component {
+  render() {
+    const { name, type, averageWeight: { value, measurementUnit }, image  } = this.props;
+    return (
+      <div className='card'>
+        <span>{name}</span>
+        <span>{type}</span>
+        <span>{`Average weight: ${value}${measurementUnit}`}</span>
+        <img src={image} alt={`An animated gif of the pokemon ${name}`}/>
+      </div>
+    )
+  }
+}
+
+// {
+//   id: 148,
+//   name: "Dragonair",
+//   type: 'Dragon',
+//   averageWeight: {
+//       value: 16.5,
+//       measurementUnit: "kg"
+//   },
+//   image: "https://cdn2.bulbagarden.net/upload/2/2c/Spr_5b_148.png",
+//   moreInfo: "https://bulbapedia.bulbagarden.net/wiki/Dragonair_(Pok%C3%A9mon)"
+// }
+
+export default Pokemon;
