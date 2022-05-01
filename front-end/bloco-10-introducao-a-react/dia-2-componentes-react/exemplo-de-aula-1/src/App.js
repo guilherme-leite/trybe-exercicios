111ca7df866d066cf7271fa18fca083aad7e2cf8
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-01 09:22:08 -0300

    Feat: Importando objetos e componente no app js e implementando

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exemplo-de-aula-1/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exemplo-de-aula-1/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Order from './Order';
 // import Image from './Image';
 // import Album from './Album';
 
@@ -31,6 +32,26 @@ import './App.css';
 //   }
 // };
 
+const headphone = {
+  id: 102,
+  user: "cena@gmail.com",
+  product: "Razer Headphone",
+  price: {
+    value: 99.99,
+    currency: "dollars"
+  }
+};
+
+const energyDrink = {
+  id: 77,
+  user: "cena@gmail.com",
+  product: "Monster 500mL",
+  price: {
+    value: 9.99,
+    currency: "dollars"
+  }
+};
+
 function App() {
   return (
     // < Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
@@ -38,7 +59,10 @@ function App() {
     //   < Album album={ album01 }/>
     //   < Album album={ album02 }/>
     // </>
-    
+    <>
+      < Order order={ headphone }/>
+      < Order order={ energyDrink }/>
+    </>
     );
 }
 
