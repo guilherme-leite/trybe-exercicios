a3ba84fa2c933b84abc9999dd55527459be5a980
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-01 10:25:42 -0300

    Fix: Descomentando pois o exemplo se refere ao outro arquivo criado previamente exercicio 01

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exemplo-de-aula-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exemplo-de-aula-2/src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 
 function App() {
-  // const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
-  // const items = shoppingList.map((item, index) => (
-  //   <li key={index}>{ item }</li>
-  // ));
-  // return (
-  //   <div>
-  //     <h2>Lista de compras</h2>
-  //     <ul>
-  //       { items }
-  //     </ul>
-  //   </div>
-  //   );
+  const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+  const items = shoppingList.map((item, index) => (
+    <li key={index}>{ item }</li>
+  ));
   return (
-    
-  )
+    <div>
+      <h2>Lista de compras</h2>
+      <ul>
+        { items }
+      </ul>
+    </div>
+    );
 }
 
 export default App;
