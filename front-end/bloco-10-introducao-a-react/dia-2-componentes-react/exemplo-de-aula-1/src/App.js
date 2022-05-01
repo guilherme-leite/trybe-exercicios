43e0584bfa33d74f4add8d46e8e0fadb2941c94d
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-01 09:16:38 -0300

    Feat: Comentando para comecar novo exemplo

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exemplo-de-aula-1/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/exemplo-de-aula-1/src/App.js
@@ -1,43 +1,44 @@
 import './App.css';
 // import Image from './Image';
-import Album from './Album';
+// import Album from './Album';
 
-const album01 = {
-  image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
-  title: 'Mylo Xyloto',
-  releaseDate: {
-    year: '2011',
-    month: '10',
-    day: '24',
-  },
-  others: {
-    recordCompany: 'Capitol, Parlophone',
-    formats: 'CD, digital'
-  }
-};
+// const album01 = {
+//   image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+//   title: 'Mylo Xyloto',
+//   releaseDate: {
+//     year: '2011',
+//     month: '10',
+//     day: '24',
+//   },
+//   others: {
+//     recordCompany: 'Capitol, Parlophone',
+//     formats: 'CD, digital'
+//   }
+// };
 
-// Declaração do objeto contendo informações do album02
-const album02 = {
-  image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
-  title: 'Ghost Stories',
-  releaseDate: {
-    year: '2014',
-    month: '05',
-    day: '16',
-  },
-  others: {
-    recordCompany: 'Parlophone',
-    formats: 'CD, digital'
-  }
-};
+// // Declaração do objeto contendo informações do album02
+// const album02 = {
+//   image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+//   title: 'Ghost Stories',
+//   releaseDate: {
+//     year: '2014',
+//     month: '05',
+//     day: '16',
+//   },
+//   others: {
+//     recordCompany: 'Parlophone',
+//     formats: 'CD, digital'
+//   }
+// };
 
 function App() {
   return (
     // < Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring'/>
-    <>
-      < Album album={ album01 }/>
-      < Album album={ album02 }/>
-    </>
+    // <>
+    //   < Album album={ album01 }/>
+    //   < Album album={ album02 }/>
+    // </>
+    
     );
 }
 
