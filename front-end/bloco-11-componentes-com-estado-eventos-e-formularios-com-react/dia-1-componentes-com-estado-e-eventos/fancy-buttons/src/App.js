6fe7edd3e401e3287be74c7a2b5c9c4c3756755a
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-02 10:29:50 -0300

    Feat: Refatorei tudo para fixar melhor

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -2,43 +2,22 @@ import React from 'react';
 
 class App extends React.Component {
   constructor() {
-    super();
+    super()
     this.state = {
-      numeroDeCliquesB1: 0,
-      numeroDeCliquesB2: 0,
-      numeroDeCliquesB3: 0,
+      valor: 0
     }
-    this.handleClick = this.handleClick.bind(this);
-    this.handleClick2 = this.handleClick2.bind(this);
-    this.handleClick3 = this.handleClick3.bind(this);
-
+    this.increaseValue = this.increaseValue.bind(this);
   }
 
-  handleClick() {
+  increaseValue() {
     this.setState((prevState, _props) => ({
-      numeroDeCliquesB1: prevState.numeroDeCliquesB1 + 1
+      valor: prevState.valor + 1,
     }))
-  };
+  }
   
-  handleClick2() {
-    this.setState((prevState, _props) => ({
-      numeroDeCliquesB2: prevState.numeroDeCliquesB2 + 1
-    }))
-  };
-  
-  handleClick3() {
-    this.setState((prevState, _props) => ({
-      numeroDeCliquesB3: prevState.numeroDeCliquesB3 + 1
-    }));
-  };
-
   render() {
-    return(
-      <div>
-      <button onClick={this.handleClick}>Botão 1 {this.state.numeroDeCliquesB1}</button>
-      <button onClick={this.handleClick2}>Botão 2 {this.state.numeroDeCliquesB2}</button>
-      <button onClick={this.handleClick3}>Botão 3 {this.state.numeroDeCliquesB3}</button>
-      </div>
+    return (
+      <button onClick={ this.increaseValue}>Contador: {this.state.valor}</button>
     );
   }
 };
