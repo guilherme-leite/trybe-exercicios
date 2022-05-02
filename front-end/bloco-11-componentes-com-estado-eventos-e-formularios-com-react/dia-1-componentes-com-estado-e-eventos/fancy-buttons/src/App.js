c2cd76c365bbb32bc44d92052ae22ad64b31bc55
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-02 07:45:25 -0300

    Feat: Adicionando mais dois botoes e outas duas funcoes com texto logado diferente

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -4,10 +4,22 @@ function handleClick() {
   console.log('Dale');
 };
 
+function handleClick2() {
+  console.log('Bom dia');
+}
+
+function handleClick3() {
+  console.log('Provolone');
+}
+
 class App extends React.Component {
   render() {
     return(
+      <div>
       <button onClick={handleClick}>Btn1</button>
+      <button onClick={handleClick2}>Btn2</button>
+      <button onClick={handleClick3}>Btn3</button>
+      </div>
     );
   }
 };
