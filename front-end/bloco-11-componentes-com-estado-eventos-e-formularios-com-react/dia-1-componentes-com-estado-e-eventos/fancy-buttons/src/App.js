aeec8ad45580440d168aaa0c6a51d1fedb0a8e75
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-02 07:41:42 -0300

    Feat: Reafatorando App.js para ser um componente de classe que renderiza um botão que chama uma funcao e loga algo

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    
+function handleClick() {
+  console.log('Dale');
+};
+
+class App extends React.Component {
+  render() {
+    return(
+      <button onClick={handleClick}></button>
     );
-}
+  }
+};
 
 export default App;
