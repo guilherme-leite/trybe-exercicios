dcce68ea206d06a7f83c24c7deb8e218bfc9b686
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-02 10:09:49 -0300

    Feat: Refatorando para que os tres botoes tenham um estado inicial e as suas funcoes foram implementadas para incrementar a contagem utilizando o setstate

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -3,28 +3,41 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
-    console.log('this no constructor', this);
+    this.state = {
+      numeroDeCliquesB1: 0,
+      numeroDeCliquesB2: 0,
+      numeroDeCliquesB3: 0,
+    }
     this.handleClick = this.handleClick.bind(this);
+    this.handleClick2 = this.handleClick2.bind(this);
+    this.handleClick3 = this.handleClick3.bind(this);
+
   }
 
   handleClick() {
-    console.log('This no handleClick', this);
-    console.log('Dale');
+    this.setState((prevState, _props) => ({
+      numeroDeCliquesB1: prevState.numeroDeCliquesB1 + 1
+    }))
   };
   
   handleClick2() {
-    console.log('Bom dia');
+    this.setState((prevState, _props) => ({
+      numeroDeCliquesB2: prevState.numeroDeCliquesB2 + 1
+    }))
   };
   
   handleClick3() {
-    console.log('Provolone');
+    this.setState((prevState, _props) => ({
+      numeroDeCliquesB3: prevState.numeroDeCliquesB3 + 1
+    }));
   };
+
   render() {
     return(
       <div>
-      <button onClick={this.handleClick}>Btn1</button>
-      <button onClick={this.handleClick2}>Btn2</button>
-      <button onClick={this.handleClick3}>Btn3</button>
+      <button onClick={this.handleClick}>Botão 1 {this.state.numeroDeCliquesB1}</button>
+      <button onClick={this.handleClick2}>Botão 2 {this.state.numeroDeCliquesB2}</button>
+      <button onClick={this.handleClick3}>Botão 3 {this.state.numeroDeCliquesB3}</button>
       </div>
     );
   }
