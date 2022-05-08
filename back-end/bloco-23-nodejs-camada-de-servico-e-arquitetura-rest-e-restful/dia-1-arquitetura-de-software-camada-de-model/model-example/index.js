2ba0c1c32111aa1a6e24e17d7a631a3147e98b70
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-08 10:16:33 -0300

    Feat: Inserindo o tratamento de dados para que se não exista nenhum author um erro seja lancado

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -12,6 +12,9 @@ app.get('/authors', async (_req, res) => {
 app.get('/authors/:id', async (req, res) => {
   const { id } = req.params;
   const author = await Author.findById(id);
+
+  if (!author) return res.status(404).json({message: 'Author not found'}); 
+
   res.status(200).json(author);
 });
 
