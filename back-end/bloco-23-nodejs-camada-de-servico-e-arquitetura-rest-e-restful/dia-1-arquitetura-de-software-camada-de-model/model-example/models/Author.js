abcbe6b9c31d72813d0511e2f3658859060a559e
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-08 10:09:55 -0300

    Feat: Criando a funcao findById que vai buscar na db os elementos que tiverem a mesma id que será passada nos params da rota. Ela já faz o serialize e devolve o objeto completo utilizando as duas funcoes

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Author.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/models/Author.js
@@ -1,3 +1,4 @@
+const { get } = require('express/lib/request');
 const connection = require('./connection');
 
 const getNewAuthor = ({ id, firstName, middleName, lastName }) => {
@@ -27,6 +28,17 @@ const getAll = async () => {
   return authors.map(serialize).map(getNewAuthor);
 };
 
+const findById = async (id) => {
+  const [author] = await connection.execute('SELECT * FROM model_example.authors WHERE id=?;', [id]);
+
+  if (author.lenght === 0) return null;
+
+  const { firstName, middleName, lastName } = author.map(serialize)[0];
+
+   return getNewAuthor({ id, firstName, middleName, lastName });
+};
+
 module.exports = {
   getAll,
+  findById,
 };
