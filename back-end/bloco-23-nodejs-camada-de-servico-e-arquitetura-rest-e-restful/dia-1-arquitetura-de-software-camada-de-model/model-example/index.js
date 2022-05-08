0263ba90272596009e040b10eb0757aba5726390
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-08 08:10:07 -0300

    Feat: Criando a rota /books no index.js

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/model-example/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
 const Author = require('./models/Author');
-
+const Books = require('./models/Books');
 const app = express();
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
   res.status(200).json(authors);
+});
+
+app.get('/books', async (_req, res) => {
+  const books = await Books.getAll();
+  res.status(200).json(books);
 });
 
 const PORT = process.env.PORT || 3000;
