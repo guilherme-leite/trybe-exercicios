6f351a5b74449252f1958312f5da0ef506872653
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-11 15:15:37 -0300

    Feat: Criando camada model e o arquivo connection.js que tera a responsabilidade de acessar o banco e exportar essa conexao

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/index.js
@@ -1,15 +1,7 @@
 const express = require('express');
-const mysql = require('mysql2/promise');
 
 const app = express();
 const PORT = 3001;
-
-const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'docker',
-  database: 'musics'
-});
 
 app.get('/musics',async (req, res) => {
 
---
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/models/connection.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2/promise');
+
+
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'docker',
+  database: 'musics'
+});
+
+module.exports = connection;
