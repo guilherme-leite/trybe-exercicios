c9546619e31de6bab79868d599851f9307aef903
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-11 15:43:13 -0300

    Feat: Utilizando a funcao createSong na rota put /musics para criar um novo elemento na tabela

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/index.js
@@ -5,10 +5,20 @@ const songsModel = require('./models/songsModel');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+
 app.get('/musics',async (req, res) => {
   const musics = await songsModel.getAll();
 
   return res.status(200).json(musics);
+});
+
+app.post('/musics', async (req, res) => {
+  const { name, album } = req.body;
+
+  const newSong = await songsModel.createSong(name, album);
+
+  return res.status(201).json(newSong);
 });
 
 app.get('/', (req, res) => res.send('Hello world!'));
