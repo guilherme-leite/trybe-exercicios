18dd93e5d77f44d836613cc2d83f86989ab284bb
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-11 15:09:22 -0300

    Feat: Importando o mysql, criando a conexao e a utilizando para retornar o onjeto na response da rota /musics

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
+
 const app = express();
 const PORT = 3001;
 
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: 'docker',
+  database: 'musics'
+});
+
+app.get('/musics',async (req, res) => {
+
+  const musics = await connection.execute('SELECT * FROM musics.songs');  
+
+  return res.status(200).json(musics);
+});
+
 app.get('/', (req, res) => res.send('Hello world!'));
-app.listen(PORT, () => console.log(`Te escuto na ${PORT}`));
+app.listen(PORT, () => console.log(`Te escuto na ${PORT}`)); 
