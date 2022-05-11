f085dc1487c03751c362be973c282ae8aa310e09
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-11 16:26:24 -0300

    Fix: Corrigindo o ocal da newSong

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/controller/songsController.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/controller/songsController.js
@@ -6,15 +6,6 @@ const musicsList = async (req, res, _next) => {
   return res.status(200).json(musics);
 };
 
-const newSong = async (name, album) => {
-  if(name === undefined || name.length <= 3) return false;
-  
-  const newMusic = await songsModel.createSong(name, album);
-
-  return newMusic;
-};
-
 module.exports = {
   musicsList,
-  newSong
 };
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/services/songsService.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/exemplo-de-aula/services/songsService.js
@@ -6,6 +6,15 @@ const getAll = () => {
   return musics;
 };
 
+const newSong = async (name, album) => {
+  if(name === undefined || name.length <= 3) return false;
+  
+  const newMusic = await songsModel.createSong(name, album);
+
+  return newMusic;
+};
+
 module.exports = {
   getAll,
+  newSong
 };
