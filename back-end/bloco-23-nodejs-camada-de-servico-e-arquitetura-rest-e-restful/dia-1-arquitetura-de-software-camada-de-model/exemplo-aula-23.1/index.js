ee44644898543843a829e5d960e606686f05e52a
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-11 08:10:13 -0300

    Feat: Implementando a rota get /char

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exemplo-aula-23.1/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exemplo-aula-23.1/index.js
@@ -9,8 +9,14 @@ const connection = mysql.createPool({
   password: 'docker',
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+app.get('/char', (req, res) => {
+  try {
+    const result = await connection.execute('SELECT * FROM live_lecture_23_1.characters');
+    return res.status(200).json(res);
+  } catch(error) {
+    console.log('internal server error', error.message);
+    return res.status(520).json({ message: 'error'});
+  }
 });
 
 app.listen(PORT, console.log(`Ouvindo na porta ${PORT}`));
