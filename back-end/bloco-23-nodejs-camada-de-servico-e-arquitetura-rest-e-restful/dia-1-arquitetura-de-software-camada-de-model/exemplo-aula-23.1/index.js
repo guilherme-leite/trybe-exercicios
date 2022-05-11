cb560cc224a5ad38975fd1580b6e792c1dc25e6d
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-11 08:06:09 -0300

    Feat: Criando connection pool

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exemplo-aula-23.1/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/exemplo-aula-23.1/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  database: 'live_lecture_23_1',
+  password: 'docker',
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World')
 });
