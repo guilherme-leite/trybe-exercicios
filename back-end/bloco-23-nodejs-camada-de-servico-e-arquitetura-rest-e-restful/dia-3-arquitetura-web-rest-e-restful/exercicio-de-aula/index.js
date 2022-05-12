e69f6ab15be5bff9923c2017c491d41989537f3e
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 11:17:53 -0300

    Feat: Utilizando um app.use na index principal para utilzar essa middleware de erro na aplicacao

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = require('./routes/index');
+const middlewares = require('./middlewares/errorMiddleware');
 
 const app = express();
 const PORT = 3001;
 
 app.use(express.json());
 app.use(router);
+app.use(middlewares.errorMiddleware);
 
 app.listen(PORT, () => console.log(`All is good my parceiro! @ ${PORT}`));
