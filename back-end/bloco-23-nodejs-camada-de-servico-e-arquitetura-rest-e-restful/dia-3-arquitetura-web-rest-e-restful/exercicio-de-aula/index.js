e08b1b32221fa008907089f2e30b1e08a569853b
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 10:55:59 -0300

    Feat: Implementando um app.use express.json() para ter acesso aos buffers

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/index.js
@@ -4,6 +4,7 @@ const router = require('./routes/index');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
 app.use(router);
 
 app.listen(PORT, () => console.log(`All is good my parceiro! @ ${PORT}`));
