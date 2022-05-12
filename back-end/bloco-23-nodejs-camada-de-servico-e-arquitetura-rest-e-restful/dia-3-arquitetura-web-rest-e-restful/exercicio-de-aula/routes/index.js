e96aed083e3320703e0d97a043cd700ee2642e6e
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 08:45:31 -0300

    Feat: Importando os index de cada uma das entidades e salvando em variáveis. Ao acessar um endpoint será roteado para a entidade compotente

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/routes/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/routes/index.js
@@ -1,5 +1,11 @@
 const express = require('express');
 
+const studentRouter = require('./students/index');
+const classRouter = require('./class/index');
+
 const router = express.router();
+
+router.use('/student', studentRouter);
+router.use('/class', classRouter);
 
 module.exports = router;
