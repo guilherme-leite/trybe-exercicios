910323191896bc38132de9391c766d54dc21aab9
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 08:46:40 -0300

    Change: Reteirando a rota padrão da index.js da aplicacao

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/index.js
@@ -2,8 +2,4 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-app.get('/', (req, res) => {
-  res.send('Bonjour');
-});
-
 app.listen(PORT, () => console.log(`All is good my parceiro! @ ${PORT}`));
