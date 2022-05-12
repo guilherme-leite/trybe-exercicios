ad2d6100c7d395bd4b7382f7523800bec1c2c33d
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 09:23:32 -0300

    Feat: utilizando a funcao listStudents do studentController

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/routes/students/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/routes/students/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const studentRouter = express.Router();
 const studentController = require('../../controller/studentController');
 
-studentRouter.get('/', studentController);
+studentRouter.get('/', studentController.listStudents);
 
 module.exports = studentRouter;
