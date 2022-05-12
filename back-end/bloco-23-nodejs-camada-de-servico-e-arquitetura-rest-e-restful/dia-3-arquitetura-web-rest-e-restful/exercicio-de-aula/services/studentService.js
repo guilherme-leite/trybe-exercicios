abca4b3c92cf5b6cd8a170957c949023ba06a917
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 10:49:53 -0300

    Feat: imporntando a classModel na studentService para utilizar o retorno da funcao classById e validar se ela existe

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/services/studentService.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/services/studentService.js
@@ -1,4 +1,5 @@
 const studentModel = require('../model/studentModel');
+const classModel = require('../model/classModel');
 
 const getStudent = async () => {
   const students = await studentModel.listAllStudents();
@@ -8,6 +9,12 @@ const getStudent = async () => {
 
 const registerStudent = async (student) => {
   const { name, age, class_id } = student;
+
+  const classExits = await classModel.classById(class_id);
+
+  if(!classExits) {
+    console.log('Classe não existe!');
+  }
 
   const registredStudent = await studentModel.createStudents(name, age, class_id);
 
