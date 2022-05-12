82283bafdc9f925baa1d50a13224a382b9e24fa7
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 10:20:28 -0300

    Feat: Criando a funcao createStudents na camada model de students e exportando

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/model/studentModel.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/model/studentModel.js
@@ -6,6 +6,18 @@ const listAllStudents = async () => {
   return result;
 };
 
+const createStudents = async (name, age, class_id) => {
+  const [result] = await connection.query('INSERT INTO students (name, age, class_id) VALUES (?,?,?)',[name, age, class_id]);
+
+  return newStudent = {
+    id: result.insertId,
+    name,
+    age,
+    class_id
+  }
+};
+
 module.exports = {
   listAllStudents,
+  createStudents
 };
