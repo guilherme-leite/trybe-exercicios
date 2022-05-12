e14e1df0cc44f5b73cee53b650500860cfdd3125
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 11:29:42 -0300

    Feat: Criando um caso de erro na errorMiddleware que vai devolver qual tipo de erro ocorreu

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/middlewares/errorMiddleware.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/middlewares/errorMiddleware.js
@@ -1,5 +1,9 @@
 module.exports = {
   errorMiddleware: (error, req, res, _next) => {
+    if (error.status) {
+      return res.status(error.status).json(error.message);
+    }
+
     return res.status(500).json({ message: 'Internal Server Error'});
   }
 };
