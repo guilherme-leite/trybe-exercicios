a2411cf965766087b39cd0790ea7b2e959e5cb58
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 11:01:30 -0300

    Feat: Criando a rota post no endpoint /new que vai tilizar a funcao da student controller para criar um novo estudante

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/routes/students/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/routes/students/index.js
@@ -4,5 +4,6 @@ const studentController = require('../../controller/studentController');
 const studentRouter = express.Router();
 
 studentRouter.get('/', studentController.listStudents);
+studentRouter.post('/new', studentController.createNewStudent);
 
 module.exports = studentRouter;
