c511739345d2858fa750e7e9df95c3ac60107416
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 09:27:10 -0300

    Fix: Corrigindo aspas na database no connection.js

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/model/connection.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/model/connection.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  database: school,
+  database: 'school',
   password: 'docker'
 });
 
