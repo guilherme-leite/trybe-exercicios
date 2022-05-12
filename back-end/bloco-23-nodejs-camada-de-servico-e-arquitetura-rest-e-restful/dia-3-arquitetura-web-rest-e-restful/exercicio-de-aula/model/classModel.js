13fb646dc8dbf086716f51770a7cc760d0d18b91
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-12 11:21:03 -0300

    Feat: Apagando o console.log da classModel que estava la para teste

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/model/classModel.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-3-arquitetura-web-rest-e-restful/exercicio-de-aula/model/classModel.js
@@ -2,7 +2,7 @@ const connection = require('./connection');
 
 const classById = async (id) => {
   const myClass = await connection.query('SELECT * FROM class WHERE id = ?', [id]);
-  console.log('CLASS:', myClass);
+
   return myClass;
 };
 
