43723c11f0305ec31b6e5a0397f42be3fde47211
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-24 12:21:56 -0300

    Fix: Limpando arquivo

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/teste/sd-017-live-lectures-lecture-24.1/services/productsService.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/teste/sd-017-live-lectures-lecture-24.1/services/productsService.js
@@ -1,33 +1,19 @@
 const { Product } = require('../models');
 
 const create = async (nome) => {
-	const newProduct = await Product.create({ nome });
 
-	return newProduct;
 };
 
 const getAll = async () => {
 
-	const products = await Product.findAll();
-
-	return products;
 };
 
 const remove = async (id) => {
 
-	await Product.destroy({
-		where: { id }
-	});
-	return true;
 };
 
 const edit = async (id, name, description) => {
 
-	await Product.update(
-		{ id, name, description },
-		{ where: { id } }
-	);
-	return true;
 };
 
 module.exports = {
