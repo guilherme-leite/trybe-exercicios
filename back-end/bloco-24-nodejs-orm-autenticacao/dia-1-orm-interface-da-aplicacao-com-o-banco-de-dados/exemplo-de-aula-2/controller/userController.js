4e8eead546f692d62f248d1a8fe21c9875f99352
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-25 17:02:50 -0300

    Feat: Criando a rota get all

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo-de-aula-2/controller/userController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo-de-aula-2/controller/userController.js
@@ -2,6 +2,17 @@ const express = require('express');
 const { User } = require('../models');
 const router = express.Router();
 
+router.get('/', async (req, res) => {
+  try {
+    const users = await User.findAll();
+
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log('erro get all', error.message);
+    res.status(error.status).json({ message: 'Internal server error'})
+  };
+});
+
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
