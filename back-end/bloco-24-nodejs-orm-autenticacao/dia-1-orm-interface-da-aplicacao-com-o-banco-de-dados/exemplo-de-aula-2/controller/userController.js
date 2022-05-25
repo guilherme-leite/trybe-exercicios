b34fbfdcebcf96c298f0105098f1096e1cbf3c58
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-25 16:44:44 -0300

    Feat: Criando a rota put para editar users

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo-de-aula-2/controller/userController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo-de-aula-2/controller/userController.js
@@ -26,9 +26,40 @@ router.get('/search/:id', async (req, res) => {
 
     return res.status(200).json(user);
   } catch (error) {
-    console.log(error.message);
+    console.log('error get /search/:id', error.message);
     res.status(500).json({ message: 'Internal Server Error' });
 };
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { fullName, email } = req.body;
+    const newUser = await User.create({ fullName, email });
+
+    return res.status(200).json(newUser);
+  } catch (error) {
+    console.log('error post /', error.message);
+    res.status(500).json({ message: 'Internal Server Error' });
+  };
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const { fullName, email } = req.body;
+    const { id } = req.params;
+
+    const [ updatedUser ] = await User.update(
+      { fullName, email },
+      { where: { id }},
+    );
+    
+    console.log('retorno updatedUser', updatedUser);
+
+    return res.status(200).json({ message: 'Usuário atualizado com sucesso!' });
+  } catch (error) {
+    console.log('error put /:id', error.message);
+    res.status(500).json({ message: 'Internal server error.'});
+  };
 });
 
 module.exports = router;
