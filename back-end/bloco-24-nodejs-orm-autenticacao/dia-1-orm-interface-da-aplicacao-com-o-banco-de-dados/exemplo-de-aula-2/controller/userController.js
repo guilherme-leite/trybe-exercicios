fcee402e8446792dec7209191dd7ab4c770abcd7
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-25 10:53:55 -0300

    Fix: Corrigindo, duas rotas no mesmo escopo por engano

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo-de-aula-2/controller/userController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exemplo-de-aula-2/controller/userController.js
@@ -16,4 +16,19 @@ router.get('/:id', async (req, res) => {
   };
 });
 
+router.get('/search/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email } = req.query;
+    const user = await User.findOne({ where: { id, email }});
+
+    if(!user) return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: 'Internal Server Error' });
+};
+});
+
 module.exports = router;
