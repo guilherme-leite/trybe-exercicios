8990dfc6d59be4738b3ba8f562fc09c7eebb4bd1
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-26 16:30:41 -0300

    Feat: Criando a rota / que que vai utilizar o BookController e nela utilizar a funcao getAll

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+
+const BookController = require('./controllers/BookController');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get('/', BookController.getAll);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
