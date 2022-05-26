0432ab23324ca621ff25de8fcc4d61d28b1b4854
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-26 16:13:28 -0300

    Feat: Criando migration para criar a tabela Books

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/migrations/20220526114244-create-books.js
@@ -0,0 +1,38 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.createTable('Books',{
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      pageQuantity: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      }
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Books');
+  }
+};
