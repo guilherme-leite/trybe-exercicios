d62facde9a031a9b1b90310ea50c6b0110dbdc4c
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-27 11:45:27 -0300

    Fix: Desestruturando o id no parametro da funcao update

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/services/BookService.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/services/BookService.js
@@ -16,7 +16,7 @@ const create = async (title, author, pageQuantity) => {
 };
 
 const update = async (id, {title, author, pageQuantity}) => {
-  const updatedBook = await Book.update({ title, author, pageQuantity }, { where: id });
+  const [updatedBook] = await Book.update({ title, author, pageQuantity }, { where: {id} });
   return updatedBook;
 };
 
