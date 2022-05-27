ff62216774411cf51e38ced0a3813716b9d4f7c3
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-27 11:36:56 -0300

    Feat: Criando a funcao update na BookService

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/services/BookService.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/services/BookService.js
@@ -15,8 +15,14 @@ const create = async (title, author, pageQuantity) => {
   return newBook;
 };
 
+const update = async (id, {title, author, pageQuantity}) => {
+  const updatedBook = await Book.update({ title, author, pageQuantity }, { where: id });
+  return updatedBook;
+};
+
 module.exports = {
   getAll,
   getById,
   create,
+  update
 };
