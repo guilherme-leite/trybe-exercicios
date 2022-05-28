eed84a6a785227b1f4a124920cdeea47b4aeb665
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-28 11:43:54 -0300

    Fix: Corrigindo typo na primaryKey

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo-1/models/Employee.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo-1/models/Employee.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const Employee = sequelize.define('Employee', {
-    id: { type: DataTypes.INTEGER, primarykey: true, autoIncrement: true },
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     age: DataTypes.INTEGER,
