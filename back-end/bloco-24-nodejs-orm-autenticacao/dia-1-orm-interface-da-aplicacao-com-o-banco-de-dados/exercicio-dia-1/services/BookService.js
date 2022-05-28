076fb2800e1cb6300b4f610d50f2b0214dd204a4
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-28 10:00:20 -0300

    Feat: Refatorando as funcoes getAll e getByAuthor para ordenar os resultados

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/services/BookService.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/services/BookService.js
@@ -1,7 +1,9 @@
 const { Book } = require('../models');
 
 const getAll = async () => {
-  const books = await Book.findAll();
+  const books = await Book.findAll({
+    order: [['title', 'ASC']],
+  });
   return books;
 };
 
@@ -27,7 +29,10 @@ const remove = async (id) => {
 };
 
 const getByAuthor = async (author) => {
-  const books = await Book.findAll({ where: { author } });
+  const books = await Book.findAll({ 
+    where: { author },
+    order: [['title', 'ASC']],
+  });
   return books;
 };
 
