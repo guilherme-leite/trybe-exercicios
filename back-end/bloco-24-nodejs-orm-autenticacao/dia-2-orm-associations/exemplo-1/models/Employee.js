b8c56760ae1a9656b79e9f0d7d9958096e065828
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-28 11:23:38 -0300

    Feat: Criando as models Address e Employee

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo-1/models/Employee.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+  const Employee = sequelize.define('Employee', {
+    id: { type: DataTypes.INTEGER, primarykey: true, autoIncrement: true },
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    age: DataTypes.INTEGER,
+  },
+  {
+    timestamps: false,
+    tableName: 'Employees',
+    underscored: true,
+  });
+
+  Employee.assossiate = (models) => {
+    Employee.hasOne(models.Address,
+      { foreignKey: 'employeeId', as: 'addresses' });
+  };
+
+  return Employee;
+};
