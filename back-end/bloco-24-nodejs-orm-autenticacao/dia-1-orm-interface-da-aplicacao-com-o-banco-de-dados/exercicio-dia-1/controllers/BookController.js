a3e144dd602dd9f81b7201e8929e6ecba0b12bcf
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-28 10:15:58 -0300

    Feat: Adicionando o parametro publisher as funcoes create e update

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/controllers/BookController.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/controllers/BookController.js
@@ -24,7 +24,7 @@ const getById = async (req, res) => {
 };
 
 const create = async (req, res) => {
-  const { title, author, pageQuantity } = req.body;
+  const { title, author, pageQuantity, publisher } = req.body;
   const newBook = await BookService.create({title, author, pageQuantity});
 
   res.status(200).json(newBook);
@@ -32,7 +32,7 @@ const create = async (req, res) => {
 
 const update = async (req, res) => {
   const { id } = req.params;
-  const { title, author, pageQuantity } = req.body;
+  const { title, author, pageQuantity, publisher } = req.body;
   const updatedBook = await BookService.update(id, {title, author, pageQuantity});
 
   if (!updatedBook) return res.status(404).json({ message: "Book not found"});
