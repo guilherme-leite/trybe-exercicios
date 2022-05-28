9c8cd3c0a3e119c03dfc2a188ac80ebe82f6553c
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-28 10:20:32 -0300

    Fix: Faltou passar o parametro nas duas funcoes do service, corrigido

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/services/BookService.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio-dia-1/services/BookService.js
@@ -12,12 +12,12 @@ const getById = async (id) => {
   return book;
 };
 
-const create = async (title, author, pageQuantity) => {
+const create = async (title, author, pageQuantity, publisher) => {
   const newBook = await Book.create(title, author, pageQuantity, publisher);
   return newBook;
 };
 
-const update = async (id, {title, author, pageQuantity}) => {
+const update = async (id, {title, author, pageQuantity, publisher}) => {
   const [updatedBook] = await Book.update({ title, author, pageQuantity, publisher }, { where: {id} });
   return updatedBook;
 };
