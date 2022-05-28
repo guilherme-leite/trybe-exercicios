8c0b0b800c150fd1dcea224edc215a53cd69d2ee
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-28 11:34:12 -0300

    Feat: Criando o servidor na porta 3000 e a rota /employees

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo-1/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { Address, Employee } = require('./models');
+
+const app = express();
+
+app.get('/employees', async (req, res) => {
+  try {
+    const employees = await Employee.findAll({
+      include: { model: Address, as: 'addresses' },
+    });
+
+    return res.status(200).json(employees);
+  } catch (error) {
+    console.log('erro rota /employees', error.message);
+    res.status(500).json({ message: 'Internal server error'});
+  };
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+});
+
+module.exports = app;
