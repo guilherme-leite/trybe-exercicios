53a963942bb2813e240f3d7af0477f23f2957280
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-29 17:21:28 -0300

    Feat: Alterando para config.js

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo-1/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "password",
+    "database": "associations",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
