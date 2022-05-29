8206dcf049a2f73fc72f992caa8cb6f081211b1d
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-29 09:32:06 -0300

    Fix: Typo na model User primarykey => primariKey. Originou o erro na field list

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo-1/models/User.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo-1/models/User.js
@@ -1,6 +1,6 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    userId: { type: DataTypes.INTEGER, primarykey: true },
+    userId: { type: DataTypes.INTEGER, primaryKey: true },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     age: DataTypes.INTEGER,
