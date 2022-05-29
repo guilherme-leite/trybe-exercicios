efd92327d6886fc9ece24806f035f566ced3ead0
Guilherme Leite <guilhermelax@hotmail.com>
2022-05-29 09:04:23 -0300

    Feat: Criando as migrationsde Book User e UserBooks

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/exemplo-1/migrations/20220529120132-create-user-books.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('UserBooks', {
+      userId: {
+        type: Sequelize.INTEGER,
+        field: 'user_id',
+        references: {
+          model: 'Users',
+          key: 'user_id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+      bookId: {
+        type: Sequelize.INTEGER,
+        field: 'book_id',
+        references: {
+          model: 'Books',
+          key: 'book_id',
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        primaryKey: true,
+      },
+    });
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('UserBooks');
+  },
+};
