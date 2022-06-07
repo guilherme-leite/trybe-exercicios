815153eb61529d4aeb3d30f597e1b0530c4b389c
Guilherme Leite <guilhermelax@hotmail.com>
2022-06-07 15:50:23 -0300

    Feat: Hello World no ts

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exemplo-de-aula-1/index.ts
+++
@@ -1,2 +0,0 @@
-let yes: boolean = true; // cria uma variável de nome "yes" e diz que o tipo é boleano e o valor é true
-let no: boolean = false; // cria uma variável de nome "no" e diz que o tipo é boleano e o valor é false
