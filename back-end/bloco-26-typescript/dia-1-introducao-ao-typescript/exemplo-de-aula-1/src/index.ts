2436946d6b811e5b6b9c308e5541b24325c14fad
Guilherme Leite <guilhermelax@hotmail.com>
2022-06-11 21:33:01 -0300

    Feat: Inferencia de tipos

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exemplo-de-aula-1/src/index.ts
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exemplo-de-aula-1/src/index.ts
@@ -14,3 +14,7 @@ let x: number;
 
 let y: number = 0;
 let z: number = 123.456;
+
+let flag = true; // o compilador irá inferir o tipo boolean
+const numberPI = 3.1416; // o compilador irá inferir o tipo number
+let message = "Hello World!"; // o compilador irá inferir o tipo string
