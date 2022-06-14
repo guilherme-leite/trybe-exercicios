a6d73963b1819ed66efb161a2280a7d06eef77b8
Guilherme Leite <guilhermelax@hotmail.com>
2022-06-14 07:07:39 -0300

    Feat: Exercicio 2 finalizado

---
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/my-ts-scripts/Months.ts
@@ -0,0 +1,16 @@
+enum Months {
+  JANUARY = 'january',
+  FEBRUARY = 'february',
+  MARCH =  'march',
+  APRIL = 'april',
+  MAY = 'may',
+  JUNE = 'june',
+  JULY = 'july',
+  AUGUST = 'august',
+  SEPTEMBER = 'september',
+  OCTOBER = 'october',
+  NOVEMBER = 'november',
+  DECEMBER = 'december'
+}
+
+export default Months;
