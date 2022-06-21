1c6813c30eb6c2afd3d363a90a8e23150724bd16
Guilherme Leite <guilhermelax@hotmail.com>
2022-06-21 07:54:26 -0300

    Feat: Crinando a superclasse Animal e o getter age

--- back-end/bloco-27-poo-solid/dia-2-heranca-e-interfaces/exemplo-de-aula1.ts
+++ back-end/bloco-27-poo-solid/dia-2-heranca-e-interfaces/exemplo-de-aula1.ts
@@ -1,41 +1,11 @@
-class Tv {
-  private _brand: string;
-  private _size: number;
-  private _resolution: string;
-  private _connections: string[];
-  private _connectedTo?: string;
+class Animal {
+  constructor(public name: string, private birthDate: Date) { }
 
-  constructor(brand: string, size: number, resolution: string, connections: string[]) {
-    this._brand = brand;
-    this._size = size;
-    this._resolution = resolution;
-    this._connections = connections;
-  }
+  get age() {
+    const timeDiff = Math.abs(
+      Date.now() - new Date(this.birthDate).getTime()
+    );
 
-  turnOn():void {
-    console.log(`${this._brand} ${this._size} ${this._brand} ${this._resolution} ${this._connections}`);
-  }
-
-  get connectedTo(): string {
-    return this._connectedTo as string;
-  }
-
-  set connectedTo(value: string) {
-    if (this._connections.includes(value)) {
-      this._connectedTo = value;
-      console.log(this._connectedTo);
-      
-    } else {
-      console.log('Sorry, connection unavailable!');
-    }
+    return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
   }
 }
-
-const tV1 = new Tv('lg', 42, 'full hd', ['hdmi', 'wifi'])
-
-tV1.turnOn();
-
-tV1.connectedTo = 'hdmi';
-
-console.log(`this tv is connected to: ${tV1.connectedTo}`);
-
