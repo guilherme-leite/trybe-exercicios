5d743ba38327ec46cdf6b170830f8c49c4dedd46
Guilherme Leite <guilhermelax@hotmail.com>
2022-06-21 08:34:44 -0300

    Feat: ALterando o encapsulamento do atributo birthDate

--- back-end/bloco-27-poo-solid/dia-2-heranca-e-interfaces/exemplo-de-aula1.ts
+++ back-end/bloco-27-poo-solid/dia-2-heranca-e-interfaces/exemplo-de-aula1.ts
@@ -1,5 +1,5 @@
 class Animal {
-  constructor(public name: string, private birthDate: Date) { }
+  constructor(public name: string, protected birthDate: Date) { }
 
   get age() {
     const timeDiff = Math.abs(
