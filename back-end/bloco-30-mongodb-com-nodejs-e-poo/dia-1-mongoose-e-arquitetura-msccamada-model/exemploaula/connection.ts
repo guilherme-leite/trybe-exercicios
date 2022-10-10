ced6fc5d29fdb5a8f25572e616bf20610f41abc6
Guilherme Leite <guilhermelax@hotmail.com>
2022-10-10 19:01:28 -0300

    connection e config

---
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-1-mongoose-e-arquitetura-msccamada-model/exemploaula/connection.ts
@@ -0,0 +1,10 @@
+import { connect } from 'mongoose';
+
+const options = {
+  user: 'user', // Usuário do banco de dados.
+  pass: 'password', // senha do usuário do banco de dados.
+  autoIndex: false, // Não cria index para cada inserção de dado no banco.
+  dbName: 'glassesStore', // Define qual banco de dados vou utilizar.
+};
+
+connect('mongodb://localhost:27017/', options);
