b06d4177df8f308eb3301eb60e29965eb3ac19b6
Guilherme Leite <guilhermelax@hotmail.com>
2022-10-20 16:11:22 -0300

    Feat: Implementando Fixture para mock

--- python/bloco_01/Dia_03:Testes/exemplo_de_aula/tests/test_data_analyzer.py
+++ python/bloco_01/Dia_03:Testes/exemplo_de_aula/tests/test_data_analyzer.py
@@ -1,8 +1,9 @@
 from src.data_analyzer import filter_titles_by_console, filter_titles_by_genre
+import pytest
 
-
-def test_filter_titles_by_console():
-	data = [
+@pytest.fixture
+def mock_data():
+    return [
         {
             "Title": "Hot Shots Golf: Open Tee",
             "Metadata": {
@@ -66,7 +67,9 @@ def test_filter_titles_by_console():
     ]
 
 
-	result = filter_titles_by_console(data, 'Nintendo DS')
+def test_filter_titles_by_console(mock_data):
+
+	result = filter_titles_by_console(mock_data, 'Nintendo DS')
 
 	assert result == [
 		'Spider-Man 2',
@@ -74,72 +77,8 @@ def test_filter_titles_by_console():
 		'WarioWare Touched!',
 	]
 
-def test_filter_titles_by_genre():
-	data = [
-        {
-            "Title": "Hot Shots Golf: Open Tee",
-            "Metadata": {
-                "Genres": "Sports",
-                "Licensed?": True,
-                "Publishers": "Sony",
-                "Sequel?": True,
-            },
-            "Release": {
-                "Console": "Sony PSP",
-                "Rating": "E",
-                "Re-release?": True,
-                "Year": 2004,
-            },
-        },
-        {
-            "Title": "Spider-Man 2",
-            "Metadata": {
-                "Genres": "Action",
-                "Licensed?": True,
-                "Publishers": "Activision",
-                "Sequel?": True,
-            },
-            "Release": {
-                "Console": "Nintendo DS",
-                "Rating": "E",
-                "Re-release?": True,
-                "Year": 2004,
-            },
-        },
-        {
-            "Title": "The Urbz: Sims in the City",
-            "Metadata": {
-                "Genres": "Simulation",
-                "Licensed?": True,
-                "Publishers": "EA",
-                "Sequel?": True,
-            },
-            "Release": {
-                "Console": "Nintendo DS",
-                "Rating": "M",
-                "Re-release?": True,
-                "Year": 2004,
-            },
-        },
-        {
-            "Title": "WarioWare Touched!",
-            "Metadata": {
-                "Genres": "Action,Racing / Driving,Sports",
-                "Licensed?": True,
-                "Publishers": "Nintendo",
-                "Sequel?": True,
-            },
-            "Release": {
-                "Console": "Nintendo DS",
-                "Rating": "E",
-                "Re-release?": True,
-                "Year": 2004,
-            },
-        },
-    ]
-
-
-	result = filter_titles_by_genre(data, 'Action')
+def test_filter_titles_by_genre(mock_data):
+	result = filter_titles_by_genre(mock_data, 'Action')
 
 	expected = ['Spider-Man 2','WarioWare Touched!']
 	
