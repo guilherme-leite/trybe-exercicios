fcdf698280893177d47fb4547221766eb6fad146
Guilherme Leite <guilhermelax@hotmail.com>
2022-12-24 14:29:31 -0300

    Feat: Aula 11

---
+++ java/java-loiane/src/cursojava/aula10/Variaveis.java
@@ -0,0 +1,17 @@
+package cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+	
+		int idade;
+		String nome;
+		String nomeDoPet;
+		
+		idade = 34;
+		
+		System.out.println("A minha idade é " + idade);
+
+	}
+
+}
