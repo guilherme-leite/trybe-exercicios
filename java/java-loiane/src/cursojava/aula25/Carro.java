0c8138165b7c197bde110b395d893dc937fb0222
Guilherme Leite <guilhermelax@hotmail.com>
2023-01-13 19:20:53 -0300

    Feat: Aula 25

---
+++ java/java-loiane/src/cursojava/aula25/Carro.java
@@ -0,0 +1,14 @@
+package cursojava.aula25;
+
+public class Carro {
+	
+	String marca;
+	String modelo;
+	int numPassageiros;
+	double capCombustivel;
+	double consumoCombustivel;
+	
+	void exibirAutonomia() {
+		System.out.println("A autonomia do carro é de " + capCombustivel * consumoCombustivel + "km");
+	}
+}
---
+++ java/java-loiane/src/cursojava/aula25/TesteCarro.java
@@ -0,0 +1,23 @@
+package cursojava.aula25;
+
+import cursojava.aula25.Carro;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		Carro van = new Carro();
+		
+		van.marca = "Fiat";
+		van.modelo = "Ducato";
+		van.numPassageiros = 10;
+		van.capCombustivel = 100;
+		van.consumoCombustivel = 0.2;
+		
+		System.out.println(van.marca);
+		System.out.println(van.modelo);
+		
+		van.exibirAutonomia();
+		
+	}
+
+}
